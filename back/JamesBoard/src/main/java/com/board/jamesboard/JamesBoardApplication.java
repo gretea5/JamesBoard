c46ff78280885efd499c24e142eb67daedbca00d
김두영 <dubblx05@naver.com>
2025-03-14 10:18:08 +0900

    [Feat] Add first project structure

---
+++ back/JamesBoard/src/main/java/com/board/jamesboard/JamesBoardApplication.java
@@ -0,0 +1,13 @@
+package com.board.jamesboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JamesBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JamesBoardApplication.class, args);
+    }
+
+}
