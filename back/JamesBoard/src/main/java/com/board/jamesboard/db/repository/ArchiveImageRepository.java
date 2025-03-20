a9f2b37fd53811300411ed111a23f683e9deecc1
김두영 <dubblx05@naver.com>
2025-03-20 17:34:19 +0900

    [Feat] Add archive repository

---
+++ back/JamesBoard/src/main/java/com/board/jamesboard/db/repository/ArchiveImageRepository.java
@@ -0,0 +1,7 @@
+package com.board.jamesboard.db.repository;
+
+import com.board.jamesboard.db.entity.ArchiveImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArchiveImageRepository extends JpaRepository<ArchiveImage, Long> {
+}
