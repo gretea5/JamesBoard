adc5032c83c05dcf1c3df1485bd36b0e17ef96e1
박장훈 <gretea4489@gmail.com>
2025-03-20 14:30:26 +0900

    [Style] Add GameImageCard.dart

---
+++ app/lib/widget/image/GameImageCard.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/cupertino.dart';
+
+class GameImageCard extends StatelessWidget {
+  final String imageUrl;
+
+  const GameImageCard({
+    super.key,
+    required this.imageUrl
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        double width = constraints.maxWidth; // 부모의 최대 너비
+        double height = width * (4 / 3); // 3:4 비율 적용
+
+        return ClipRRect(
+          borderRadius: BorderRadius.circular(4.0),
+          child: Image.asset(
+            imageUrl,
+            width: width,
+            height: height,
+            fit: BoxFit.cover,
+          ),
+        );
+      },
+    );
+  }
+}
