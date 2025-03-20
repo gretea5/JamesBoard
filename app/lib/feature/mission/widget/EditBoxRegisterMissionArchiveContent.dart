e46a72d6ecd8ea04adb4077ce6c738f05922c0b4
hyuun <wotj28@naver.com>
2025-03-20 16:54:14 +0900

    [Style] Add EditBoxRegisterMissionArchiveContent.dart #front

---
+++ app/lib/feature/mission/widget/EditBoxRegisterMissionArchiveContent.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:jamesboard/theme/Colors.dart';
+
+class EditBoxRegisterMissionArchiveContent extends StatefulWidget {
+  const EditBoxRegisterMissionArchiveContent({super.key});
+
+  @override
+  State<EditBoxRegisterMissionArchiveContent> createState() =>
+      _CustomInputBoxState();
+}
+
+class _CustomInputBoxState extends State<EditBoxRegisterMissionArchiveContent> {
+  final TextEditingController _controller = TextEditingController();
+  bool _hasInput = false;
+
+  void initState() {
+    super.initState();
+
+    _controller.addListener(() {
+      setState(() {
+        _hasInput = _controller.text.isNotEmpty;
+      });
+    });
+  }
+
+  // 메모리 누수 방지.
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: BoxConstraints(
+        maxWidth: MediaQuery.of(context).size.width,
+      ),
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+      decoration: BoxDecoration(
+        color: secondaryBlack,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: TextField(
+        controller: _controller,
+        keyboardType: TextInputType.text,
+        minLines: 3,
+        maxLines: null,
+        inputFormatters: [
+          LengthLimitingTextInputFormatter(255),
+        ],
+        style: TextStyle(
+          color: _hasInput ? mainWhite : mainGrey,
+          fontSize: 16,
+          fontFamily: 'PretendardMedium',
+        ),
+        decoration: InputDecoration(
+            hintText: '문구 입력',
+            hintStyle: TextStyle(
+                color: mainGrey, fontSize: 16, fontFamily: 'PretendardMedium'),
+            border: InputBorder.none,
+            isDense: true,
+            contentPadding: EdgeInsets.zero),
+      ),
+    );
+  }
+}
