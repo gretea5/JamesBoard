67e3d5ab0d69c87f9a09893444e14f584de1c891
hyuun <wotj28@naver.com>
2025-03-20 09:30:41 +0900

    [Style] Add Colors.dart #front

--- app/lib/theme/Colors.dart
+++ app/lib/theme/Colors.dart
@@ -1,3 +1,9 @@
 import 'package:flutter/material.dart';
 
 // Color themeColor = Color.fromARGB(255, 79, 112, 229);
+Color mainBlack = Color(0xFF0C0F14);
+Color secondaryBlack = Color(0xFF25282D);
+Color mainGrey = Color(0xFF89919A);
+Color mainWhite = Color(0xFFFAFAFA);
+Color mainRed = Color(0xFF5E0909);
+Color mainGold = Color(0xFFD6B346);
