68e30152199af8bb4c791a85c9a36e932f3dce8a
박장훈 <gretea4489@gmail.com>
2025-03-21 14:59:15 +0900

    [Style] Add GameImageMyPageCard.dart

---
+++ app/lib/widget/image/GameImageMyPageCard.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class GameImageMyPageCard extends StatelessWidget {
+  final String imageUrl;
+
+  const GameImageMyPageCard({
+    super.key,
+    required this.imageUrl
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        double width = constraints.maxWidth; // 부모의 최대 너비
+        double height = width * (3 / 3); // 3:4 비율 적용
+
+        return ClipRRect(
+          borderRadius: BorderRadius.circular(4.0),
+          child: Image.asset(
+            imageUrl,
+            width: width,
+            height: height,
+            fit: BoxFit.cover,
+          ),
+        );
+      },
+    );
+  }
+}
