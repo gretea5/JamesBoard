bfd39366f79ed4c72ba6529e5018e80345dacdea
박장훈 <gretea4489@gmail.com>
2025-03-24 11:13:34 +0900

    [Style] BottomSheet 하단 Divider 주기 #front

--- app/lib/widget/bottomsheet/BottomSheetCommonFilter.dart
+++ app/lib/widget/bottomsheet/BottomSheetCommonFilter.dart
@@ -64,15 +64,23 @@ class _BottomSheetCommonFilterState extends State<BottomSheetCommonFilter> {
             onTap: () => Navigator.pop(context),
             behavior: HitTestBehavior.opaque,
             child: Container(
+              decoration: const BoxDecoration(
+                color: secondaryBlack,
+                border: Border(
+                  top: BorderSide(
+                    color: mainGrey,
+                    width: 1.0
+                  )
+                )
+              ),
               padding: const EdgeInsets.symmetric(vertical: 16),
               alignment: Alignment.center,
-              color: secondaryBlack,
               child: Text(
                 '닫기',
                 style: TextStyle(
-                    fontSize: 16,
-                    color: mainWhite,
-                    fontFamily: 'PretendardBold'
+                  fontSize: 16,
+                  color: mainWhite,
+                  fontFamily: 'PretendardBold'
                 ),
               ),
             ),
