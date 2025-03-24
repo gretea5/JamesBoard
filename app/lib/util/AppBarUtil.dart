8cedf054b05fc1fd21a94e9bc628fffeb4a1bd71
박장훈 <gretea4489@gmail.com>
2025-03-24 21:24:46 +0900

    [Feat] index 마다 AppBar 리턴 함수 메소드 추가

---
+++ app/lib/util/AppBarUtil.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:jamesboard/feature/boardgame/widget/appbar/MyPageAppBar.dart';
+
+import '../feature/boardgame/widget/appbar/HomeAppBar.dart';
+import '../feature/boardgame/widget/appbar/ListAppBar.dart';
+
+class AppBarUtil {
+  static PreferredSizeWidget? getAppBar(int selectedIndex) {
+    switch (selectedIndex) {
+      case 0: // 홈
+        return HomeAppBar(title: "JamesBoard");
+      case 1: // 검색 (검색 바가 있는 AppBar)
+        return ListAppBar(title: "요원님을 위한 맞춤 업무");
+      case 2: // 프로필 (AppBar가 없음)
+        return null;
+      case 3: // 프로필 (AppBar가 없음)
+        return ListAppBar(title: "임무 보고 아카이브");
+      default:
+        return MyPageAppBar(title: "요원 로그");
+    }
+  }
+}
