6eafb4caa74a8e84edbc23fcfb46076d1596a9b6
hyuun <wotj28@naver.com>
2025-03-24 15:42:03 +0900

    [Style] Fix ImageItemRegisterMission.dart width, height 변경 #front

--- app/lib/feature/mission/widget/ImageItemRegisterMission.dart
+++ app/lib/feature/mission/widget/ImageItemRegisterMission.dart
@@ -1,29 +1,65 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 import '../../../theme/Colors.dart';
 
 class ImageItemRegisterMission extends StatelessWidget {
   final String imageUrl;
+  final VoidCallback onRemove;
 
-  const ImageItemRegisterMission({super.key, required this.imageUrl});
+  const ImageItemRegisterMission({
+    super.key,
+    required this.imageUrl,
+    required this.onRemove,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        double size = (constraints.maxWidth - 16) / 2;
-
-        return Container(
-          width: size * 0.3,
-          height: size * 0.3,
+    return Stack(
+      alignment: Alignment.topRight,
+      children: [
+        Container(
+          width: 150, // 고정 크기 설정
+          height: 150,
           decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              image: DecorationImage(
-                image: AssetImage(imageUrl),
-                fit: BoxFit.cover,
-              )),
-        );
-      },
+            borderRadius: BorderRadius.circular(10),
+            image: DecorationImage(
+              image: _getImageProvider(imageUrl), // FileImage 지원 추가
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        Positioned(
+          top: -6,
+          right: -6,
+          child: GestureDetector(
+            onTap: onRemove,
+            child: Container(
+              padding: const EdgeInsets.all(4),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: mainBlack.withOpacity(0.7),
+              ),
+              child: const Icon(
+                Icons.close,
+                color: mainGold,
+                size: 16,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
+  }
+
+  // 파일인지 에셋인지 구분해서 이미지 반환
+  ImageProvider _getImageProvider(String imageUrl) {
+    if (imageUrl.startsWith('/')) {
+      return FileImage(File(imageUrl));
+    } else {
+      return AssetImage(imageUrl);
+    }
   }
 }
