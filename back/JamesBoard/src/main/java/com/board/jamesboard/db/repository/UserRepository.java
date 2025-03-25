57365f8d7afb482dd3713addfd1dafce274081dc
Minseok <ro8363@naver.com>
2025-03-25 17:39:21 +0900

    [Temp] 임시 커밋

--- back/JamesBoard/src/main/java/com/board/jamesboard/db/repository/UserRepository.java
+++ back/JamesBoard/src/main/java/com/board/jamesboard/db/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.board.jamesboard.db.repository;
 
+import com.board.jamesboard.db.entity.Archive;
 import com.board.jamesboard.db.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -16,5 +18,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUserId(Long userId);
 
+//    List<Archive> findByUserIdAndGameIdOrderByCreatedAtDesc(Long userId, Long gameId);
 
 }
