f612d2506309af3e475fa190485cf93eb1159097
kdh4718 <charlie7276@naver.com>
2025-03-26 20:47:14 +0900

    [Feat] TextFieldChatBotMessage TextField 예외 처리 - 최대 길이 지정, 공백만, 특수 기호만, 이모지만, 제2 외국어(일본어, 중국어 등) 입력 불가 #front

--- app/lib/feature/chatbot/widget/TextFieldChatBotMessage.dart
+++ app/lib/feature/chatbot/widget/TextFieldChatBotMessage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:jamesboard/theme/Colors.dart';
 import 'package:logger/logger.dart';
+import 'package:flutter/services.dart';
 
 class TextFieldChatBotMessage extends StatefulWidget {
   const TextFieldChatBotMessage({super.key});
@@ -15,11 +16,40 @@ class _TextFieldChatBotMessageState extends State<TextFieldChatBotMessage> {
   final TextEditingController _controller = TextEditingController();
   final Logger logger = Logger();
 
+  // 문자 처리 - 공백만, 특수 기호만 입력, 이모지, 외국어(일본어, 중국어 등) 입력 불가
+  bool _isValidInput(String text) {
+    final regex = RegExp(r'[a-zA-Z0-9가-힣]');
+    return regex.hasMatch(text.trim());
+  }
+
+  // ✅ 메시지 전송 함수
   void _onSend() {
-    if (_controller.text.trim().isNotEmpty) {
-      logger.d("ChatBot: ${_controller.text}");
+    String message = _controller.text.trim();
+
+    if (message.isNotEmpty && _isValidInput(message)) {
+      logger.d("ChatBot: $message");
+
+      FocusScope.of(context).unfocus();
+
+      // SnackBar 표시
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('메시지 전송: $message'),
+          duration: Duration(seconds: 2),
+        ),
+      );
+
       _controller.clear();
-      FocusScope.of(context).unfocus(); // 키패드 내리기
+    } else {
+      // 유효하지 않은 입력
+      FocusScope.of(context).unfocus();
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('올바른 메시지를 입력하세요!'),
+          duration: Duration(seconds: 2),
+          backgroundColor: mainRed,
+        ),
+      );
     }
   }
 
@@ -40,20 +70,24 @@ class _TextFieldChatBotMessageState extends State<TextFieldChatBotMessage> {
         children: [
           Expanded(
             child: TextField(
+              maxLength: 100, // 최대 길이 설정
+              inputFormatters: [LengthLimitingTextInputFormatter(100)],
               controller: _controller,
               decoration: const InputDecoration(
                 hintText: '메시지 입력',
                 border: InputBorder.none,
+                counterText: '',
               ),
               style: TextStyle(
                 color: mainGrey,
                 fontSize: 16,
               ),
-              onEditingComplete: _onSend, // 키패드 완료 버튼 눌렀을 때 호출
+              onEditingComplete: _onSend, // 키패드 완료 버튼
             ),
           ),
+          // 전송 버튼 (SVG 아이콘)
           GestureDetector(
-            onTap: _onSend, // 이미지 클릭 시 호출
+            onTap: _onSend,
             child: SvgPicture.asset(
               'assets/image/icon_arrow_up.svg',
               width: 34,
