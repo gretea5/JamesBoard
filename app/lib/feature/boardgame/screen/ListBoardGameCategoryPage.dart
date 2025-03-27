70b8448b692d32745d7c79545da072349a7e2c46
박장훈 <gretea4489@gmail.com>
2025-03-27 15:24:56 +0900

    [Feat] 보드게임 필터링 설정 구현
    Former-commit-id: 02fe1c3a014c1e03ae6354c02f11ec132c5aed12

--- app/lib/feature/boardgame/screen/ListBoardGameCategoryPage.dart
+++ app/lib/feature/boardgame/screen/ListBoardGameCategoryPage.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:jamesboard/theme/Colors.dart';
 import '../../../widget/button/ButtonCommonFilter.dart';
 import '../../../widget/bottomsheet/BottomSheetCommonFilter.dart';
+import '../../../widget/image/ImageCommonGameCard.dart';
 
 class ListBoardGameCategoryPage extends StatefulWidget {
   const ListBoardGameCategoryPage({super.key});
 
   @override
-  State<ListBoardGameCategoryPage> createState() => _ListBoardGameCategoryPageState();
+  State<ListBoardGameCategoryPage> createState() =>
+      _ListBoardGameCategoryPageState();
 }
 
 class _ListBoardGameCategoryPageState extends State<ListBoardGameCategoryPage> {
@@ -14,13 +17,46 @@ class _ListBoardGameCategoryPageState extends State<ListBoardGameCategoryPage> {
     '장르': '장르',
     '인원': '인원',
     '난이도': '난이도',
+    '평균 게임 시간': '평균 게임 시간'
   };
 
   final Map<String, List<String>> filterOptions = {
     '장르': ['파티', '전략', '경제', '모험', '롤플레잉', '가족', '추리', '전쟁', '추상전략', '상관없음'],
     '인원': ['Solo: 1인', 'Duo: 2인', 'Team: 3~4인', 'Assemble: 5인 이상', '상관없음'],
     '난이도': ['초급', '중급', '고급', '상관없음'],
+    '평균 게임 시간': [
+      '초신속 임무 (0 ~ 30분)',
+      '정밀 작전 (60 ~ 120분)',
+      '장기 작전 (120 ~ 240분)',
+      '마스터 작전 (240분 이상)',
+      '상관없음'
+    ],
   };
+
+  // 이미지 URL 리스트
+  final List<String> imageUrls = [
+    'https://cf.geekdo-images.com/x3zxjr-Vw5iU4yDPg70Jgw__original/img/FpyxH41Y6_ROoePAilPNEhXnzO8=/0x0/filters:format(jpeg)/pic3490053.jpg',
+    'https://cf.geekdo-images.com/7SrPNGBKg9IIsP4UQpOi8g__original/img/GKueTbkCk2Ramf6ai8mDj-BP6cI=/0x0/filters:format(jpeg)/pic4325841.jpg',
+    'https://cf.geekdo-images.com/5CFwjd8zTcGYVUnkXh04hw__original/img/N8btACZrnEYK1amBNk26VBdcGwc=/0x0/filters:format(jpeg)/pic1176894.jpg',
+    'https://cf.geekdo-images.com/x3zxjr-Vw5iU4yDPg70Jgw__original/img/FpyxH41Y6_ROoePAilPNEhXnzO8=/0x0/filters:format(jpeg)/pic3490053.jpg',
+    'https://cf.geekdo-images.com/7SrPNGBKg9IIsP4UQpOi8g__original/img/GKueTbkCk2Ramf6ai8mDj-BP6cI=/0x0/filters:format(jpeg)/pic4325841.jpg',
+    'https://cf.geekdo-images.com/5CFwjd8zTcGYVUnkXh04hw__original/img/N8btACZrnEYK1amBNk26VBdcGwc=/0x0/filters:format(jpeg)/pic1176894.jpg',
+    'https://cf.geekdo-images.com/x3zxjr-Vw5iU4yDPg70Jgw__original/img/FpyxH41Y6_ROoePAilPNEhXnzO8=/0x0/filters:format(jpeg)/pic3490053.jpg',
+    'https://cf.geekdo-images.com/7SrPNGBKg9IIsP4UQpOi8g__original/img/GKueTbkCk2Ramf6ai8mDj-BP6cI=/0x0/filters:format(jpeg)/pic4325841.jpg',
+    'https://cf.geekdo-images.com/5CFwjd8zTcGYVUnkXh04hw__original/img/N8btACZrnEYK1amBNk26VBdcGwc=/0x0/filters:format(jpeg)/pic1176894.jpg',
+    'https://cf.geekdo-images.com/7SrPNGBKg9IIsP4UQpOi8g__original/img/GKueTbkCk2Ramf6ai8mDj-BP6cI=/0x0/filters:format(jpeg)/pic4325841.jpg',
+    'https://cf.geekdo-images.com/5CFwjd8zTcGYVUnkXh04hw__original/img/N8btACZrnEYK1amBNk26VBdcGwc=/0x0/filters:format(jpeg)/pic1176894.jpg',
+    'https://cf.geekdo-images.com/7SrPNGBKg9IIsP4UQpOi8g__original/img/GKueTbkCk2Ramf6ai8mDj-BP6cI=/0x0/filters:format(jpeg)/pic4325841.jpg',
+    'https://cf.geekdo-images.com/5CFwjd8zTcGYVUnkXh04hw__original/img/N8btACZrnEYK1amBNk26VBdcGwc=/0x0/filters:format(jpeg)/pic1176894.jpg',
+    'https://cf.geekdo-images.com/x3zxjr-Vw5iU4yDPg70Jgw__original/img/FpyxH41Y6_ROoePAilPNEhXnzO8=/0x0/filters:format(jpeg)/pic3490053.jpg',
+    'https://cf.geekdo-images.com/7SrPNGBKg9IIsP4UQpOi8g__original/img/GKueTbkCk2Ramf6ai8mDj-BP6cI=/0x0/filters:format(jpeg)/pic4325841.jpg',
+    'https://cf.geekdo-images.com/5CFwjd8zTcGYVUnkXh04hw__original/img/N8btACZrnEYK1amBNk26VBdcGwc=/0x0/filters:format(jpeg)/pic1176894.jpg',
+    'https://cf.geekdo-images.com/7SrPNGBKg9IIsP4UQpOi8g__original/img/GKueTbkCk2Ramf6ai8mDj-BP6cI=/0x0/filters:format(jpeg)/pic4325841.jpg',
+    'https://cf.geekdo-images.com/5CFwjd8zTcGYVUnkXh04hw__original/img/N8btACZrnEYK1amBNk26VBdcGwc=/0x0/filters:format(jpeg)/pic1176894.jpg',
+    'https://cf.geekdo-images.com/x3zxjr-Vw5iU4yDPg70Jgw__original/img/FpyxH41Y6_ROoePAilPNEhXnzO8=/0x0/filters:format(jpeg)/pic3490053.jpg',
+    'https://cf.geekdo-images.com/7SrPNGBKg9IIsP4UQpOi8g__original/img/GKueTbkCk2Ramf6ai8mDj-BP6cI=/0x0/filters:format(jpeg)/pic4325841.jpg',
+    'https://cf.geekdo-images.com/5CFwjd8zTcGYVUnkXh04hw__original/img/N8btACZrnEYK1amBNk26VBdcGwc=/0x0/filters:format(jpeg)/pic1176894.jpg',
+  ];
 
   void _showFilterBottomSheet(String filterType) async {
     final result = await showModalBottomSheet<String>(
@@ -28,7 +64,9 @@ class _ListBoardGameCategoryPageState extends State<ListBoardGameCategoryPage> {
       backgroundColor: Colors.transparent,
       builder: (context) => BottomSheetCommonFilter(
         items: filterOptions[filterType]!,
-        initialValue: selectedFilters[filterType] != filterType ? selectedFilters[filterType] : null,
+        initialValue: selectedFilters[filterType] != filterType
+            ? selectedFilters[filterType]
+            : null,
       ),
     );
 
@@ -41,19 +79,69 @@ class _ListBoardGameCategoryPageState extends State<ListBoardGameCategoryPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Wrap(
-          spacing: 20.0,
-          children: selectedFilters.keys.map((filterType) {
-            return ButtonCommonFilter(
-              text: selectedFilters[filterType]!,
-              isSelected: selectedFilters[filterType] != filterType,
-              onTap: () => _showFilterBottomSheet(filterType),
-            );
-          }).toList(),
-        ),
-      ],
+    return SingleChildScrollView(
+      // 스크롤 가능하도록 감싸줌
+      child: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(left: 20, right: 20, top: 16),
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  ...selectedFilters.keys.map((filterType) {
+                    return Container(
+                      margin: EdgeInsets.only(right: 16),
+                      child: ButtonCommonFilter(
+                        text: selectedFilters[filterType]!,
+                        isSelected: selectedFilters[filterType] != filterType,
+                        onTap: () => _showFilterBottomSheet(filterType),
+                      ),
+                    );
+                  }),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        // 모든 필터를 기본값(키 값)으로 초기화
+                        selectedFilters.updateAll((key, value) => key);
+                      });
+                    },
+                    child: Text(
+                      '초기화',
+                      style: TextStyle(
+                        color: mainWhite,
+                        fontSize: 16,
+                        fontFamily: 'PretendardSemiBold',
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(height: 16),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20),
+            child: GridView.builder(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3, // 한 줄에 3개
+                crossAxisSpacing: 12,
+                mainAxisSpacing: 12,
+                childAspectRatio: 3 / 4,
+              ),
+              itemCount: imageUrls.length,
+              itemBuilder: (context, index) {
+                return ImageCommonGameCard(
+                  imageUrl: imageUrls[index],
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
