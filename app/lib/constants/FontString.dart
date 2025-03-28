74d58b6772ee1c237f233265a074d207caf36810
박장훈 <gretea4489@gmail.com>
2025-03-28 10:09:10 +0900

    [Feat] 폰트 스타일 클래스 추가 #front
    Former-commit-id: b5f0da4518d71648cd5f95bbee3558c19cc17f13

---
+++ app/lib/constants/FontString.dart
@@ -0,0 +1,5 @@
+class FontString {
+  static const String pretendardBold = "PretendardBold";
+  static const String pretendardSemiBold = "PretendardSemiBold";
+  static const String pretendardMedium = "PretendardMedium";
+}
