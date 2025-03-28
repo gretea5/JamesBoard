1b7123c0eb482306d52a7eca1218862b4426d98b
hyuun <wotj28@naver.com>
2025-03-29 00:16:24 +0900

    [Style] Add Scaffold on LoginScreen.dart #front
    Former-commit-id: a74f6c9d91c875720d59118031728cc17c0cd48e

--- app/lib/feature/login/screen/LoginScreen.dart
+++ app/lib/feature/login/screen/LoginScreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:jamesboard/feature/login/viewmodel/LoginViewModel.dart';
 import 'package:jamesboard/repository/LoginRepository.dart';
+import 'package:jamesboard/theme/Colors.dart';
 import '../widget/CardLoginExplanation.dart';
 import '../widget/KakaoLoginButton.dart';
 
@@ -15,23 +16,26 @@ class _LoginScreenState extends State<LoginScreen> {
   final viewModel = LoginViewModel(LoginRepository.create());
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 32,
-          ),
-          Flexible(
-            child: CardLoginExplanation(),
-          ),
-          KakaoLoginButton(onPressed: () async {
-            await viewModel.login(context);
-          }),
-          SizedBox(
-            height: 20,
-          ),
-        ],
+    return Scaffold(
+      backgroundColor: mainBlack,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 32,
+            ),
+            Flexible(
+              child: CardLoginExplanation(),
+            ),
+            KakaoLoginButton(onPressed: () async {
+              await viewModel.login(context);
+            }),
+            SizedBox(
+              height: 20,
+            ),
+          ],
+        ),
       ),
     );
   }
