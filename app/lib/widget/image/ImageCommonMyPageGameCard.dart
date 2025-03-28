25592b693b7f450931bb7570c5fb26084e51b592
kdh4718 <charlie7276@naver.com>
2025-03-28 10:51:56 +0900

    [Style] Design Tab - 임무 보고 #front
    Former-commit-id: 187b7e943a4d623c789607e8b2ad0fdc9cba1516

--- app/lib/widget/image/ImageCommonMyPageGameCard.dart
+++ app/lib/widget/image/ImageCommonMyPageGameCard.dart
@@ -1,27 +1,37 @@
 import 'package:flutter/material.dart';
 
 class ImageCommonMyPageGameCard extends StatelessWidget {
-  final String imageUrl;
+  final List<Map<String, String>> images;
+  final Function(String id) onTap;
 
   const ImageCommonMyPageGameCard({
     super.key,
-    required this.imageUrl
+    required this.images,
+    required this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        double width = constraints.maxWidth; // 부모의 최대 너비
-        double height = width * (3 / 3); // 3:4 비율 적용
+    return GridView.builder(
+      padding: const EdgeInsets.all(8.0),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3, // 3x3 그리드
+        crossAxisSpacing: 8.0, // 좌우 간격
+        mainAxisSpacing: 8.0, // 상하 간격
+        childAspectRatio: 1, // 정사각형 비율
+      ),
+      itemCount: images.length,
+      itemBuilder: (context, index) {
+        final item = images[index];
 
-        return ClipRRect(
-          borderRadius: BorderRadius.circular(10.0),
-          child: Image.asset(
-            imageUrl,
-            width: width,
-            height: height,
-            fit: BoxFit.cover,
+        return GestureDetector(
+          onTap: () => onTap(item['id']!), // 클릭 시 id 전달
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(10.0),
+            child: Image.network(
+              item['img']!,
+              fit: BoxFit.cover,
+            ),
           ),
         );
       },
