e36592abee045ac349b334044785533fb6ebc553
hyuun <wotj28@naver.com>
2025-03-28 10:11:32 +0900

    [Feat] Add LoginService.dart #front
    Former-commit-id: c2ad397cb58512a77f860eb61612a1cbf2d5319f

---
+++ app/lib/datasource/api/LoginService.dart
@@ -0,0 +1,27 @@
+import 'package:dio/dio.dart';
+import 'package:jamesboard/datasource/model/response/RenewalAccessTokenResponse.dart';
+import 'package:retrofit/error_logger.dart';
+import 'package:retrofit/http.dart';
+
+import '../model/request/RenewalAccessTokenRequest.dart';
+
+part 'LoginService.g.dart';
+
+@RestApi(baseUrl: "https://j12d205.p.ssafy.io/")
+abstract class LoginService {
+  factory LoginService(Dio dio, {String baseUrl}) = _LoginService;
+
+  // 카카오 로그인 페이지 리다이렉트
+  @GET("/api/auth/login-oauth")
+  Future<void> goToKakaoLoginPage();
+
+  // 로그아웃
+  @POST("/api/auth/logout")
+  Future<void> logout();
+
+  // AccessToken 갱신
+  @POST("api/auth/refresh")
+  Future<RenewalAccessTokenResponse> renewalAccessToken(
+    @Body() RenewalAccessTokenRequest request,
+  );
+}
