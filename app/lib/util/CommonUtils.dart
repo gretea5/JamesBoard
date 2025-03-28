f22248948336b68bdd7f8d889744872a74068f10
kdh4718 <charlie7276@naver.com>
2025-03-28 10:59:56 +0900

    [Feat] Add CommonUtils getGenreColor #front
    Former-commit-id: 222c10ee06a87908736d321aa77d20755165f67f

--- app/lib/util/CommonUtils.dart
+++ app/lib/util/CommonUtils.dart
@@ -1,4 +1,9 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+
+import '../theme/Colors.dart';
 
 class CommonUtils {
   // 시간을 '오전 08:13' 형식으로 반환하는 메소드
@@ -10,5 +15,31 @@ class CommonUtils {
     formattedTime = formattedTime.replaceFirst('AM', '오전').replaceFirst('PM', '오후');
 
     return formattedTime;
+  }
+
+  // 장르에 맞는 색상을 반환하는 메소드
+  static Color getGenreColor(String genre) {
+    switch (genre) {
+      case '전략':
+        return mainStrategy;
+      case '파티':
+        return mainParty;
+      case '추리':
+        return mainReasoning;
+      case '경제':
+        return mainEconomy;
+      case '모험':
+        return mainAdventure;
+      case '롤플레잉':
+        return mainRolePlaying;
+      case '가족':
+        return mainFamily;
+      case '전쟁':
+        return mainWar;
+      case '추상전략':
+        return mainAbstractStrategy;
+      default:
+        return Colors.black; // 기본 색상 (알 수 없는 장르에 대한 색상)
+    }
   }
 }
