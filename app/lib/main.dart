cdb985db4bdffdd5318bac1904921927bc326007
hyuun <wotj28@naver.com>
2025-03-28 16:18:43 +0900

    [Feat] 앱 실행 시 로그인 여부 판별 #front
    Former-commit-id: 1323b06b386f1ba057c0d20e022fd40300883e7a

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -5,11 +5,15 @@ import 'package:jamesboard/feature/boardgame/screen/BoardGameHomeScreen.dart';
 import 'package:jamesboard/feature/boardgame/screen/ListArchieveScreenEx.dart';
 import 'package:jamesboard/feature/boardgame/screen/MyPageScreenEx.dart';
 import 'package:jamesboard/feature/boardgame/screen/RecommGameScreenEx.dart';
+import 'package:jamesboard/feature/login/screen/LoginScreen.dart';
 import 'package:jamesboard/feature/mission/screen/MissionEditScreen.dart';
 import 'package:jamesboard/util/AppBarUtil.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 import 'package:logger/logger.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:jamesboard/theme/Colors.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:kakao_flutter_sdk_common/kakao_flutter_sdk_common.dart';
 
 final logger = Logger(
     printer: PrettyPrinter(
@@ -18,19 +22,34 @@ final logger = Logger(
   printTime: true,
 ));
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  SystemChrome.setPreferredOrientations([
+  KakaoSdk.init(nativeAppKey: 'd6a47244edea272d361af14d6d35d274');
+
+  final keyHash = await KakaoSdk.origin;
+  logger.d('현재 사용 중인 키 해시 : $keyHash');
+
+  await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
-  ]).then((_) {
-    runApp(MyApp());
-  });
+  ]);
+
+  final prefs = await SharedPreferences.getInstance();
+  final accessToken = prefs.getString('accessToken');
+
+  final isLoggedIn = accessToken != null && accessToken.isNotEmpty;
+
+  runApp(MyApp(isLoggedIn: isLoggedIn));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final bool isLoggedIn;
+
+  const MyApp({
+    super.key,
+    required this.isLoggedIn,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -42,13 +61,15 @@ class MyApp extends StatelessWidget {
         );
       },
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'JamesBoard',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
         splashColor: Colors.transparent, // 클릭 시 원형 퍼지는 효과 제거
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: isLoggedIn
+          ? const MyHomePage(title: 'Flutter Demo Home Page')
+          : const LoginScreen(),
     );
   }
 }
@@ -99,68 +120,73 @@ class _MyHomePageState extends State<MyHomePage> {
       body: _pages[_selectedIndex],
       backgroundColor: mainBlack, // 배경색 검정으로 설정
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-        backgroundColor: mainBlack,
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        selectedItemColor: mainWhite,
-        unselectedItemColor: mainWhite,
-        type: BottomNavigationBarType.fixed,
-        items: [
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset('assets/image/icon_home_unselected.svg',
-              width: 24,
-              height: 24,
-              colorFilter: ColorFilter.mode(mainWhite, BlendMode.srcIn)),
-            activeIcon: SvgPicture.asset('assets/image/icon_home_selected.svg',
-              width: 24,
-              height: 24,
-              colorFilter: ColorFilter.mode(mainWhite, BlendMode.srcIn)),
-            label: 'home',
-          ),
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset('assets/image/icon_recommend_unselected.svg',
-                width: 24,
-                height: 24,
-                colorFilter: ColorFilter.mode(mainWhite, BlendMode.srcIn)),
-            activeIcon: SvgPicture.asset('assets/image/icon_recommend_selected.svg',
-                width: 24,
-                height: 24,
-                colorFilter: ColorFilter.mode(mainWhite, BlendMode.srcIn)),
-            label: 'recommend',
-          ),
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset('assets/image/icon_register_unselected.svg',
-                width: 24,
-                height: 24,
-                colorFilter: ColorFilter.mode(mainWhite, BlendMode.srcIn)),
-            label: 'search',
-          ),
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset('assets/image/icon_archive_unselected.svg',
-                width: 24,
-                height: 24,
-                colorFilter: ColorFilter.mode(mainWhite, BlendMode.srcIn)),
-            activeIcon: SvgPicture.asset('assets/image/icon_archive_selected.svg',
-                width: 24,
-                height: 24,
-                colorFilter: ColorFilter.mode(mainWhite, BlendMode.srcIn)),
-            label: 'recommend',
-          ),
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset('assets/image/icon_mypage_unselected.svg',
-                width: 24,
-                height: 24,
-                colorFilter: ColorFilter.mode(mainWhite, BlendMode.srcIn)),
-            activeIcon: SvgPicture.asset('assets/image/icon_mypage_selected.svg',
-                width: 24,
-                height: 24,
-                colorFilter: ColorFilter.mode(mainWhite, BlendMode.srcIn)),
-            label: 'mypage',
-          ),
-        ]
-      ),
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+          backgroundColor: mainBlack,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          selectedItemColor: mainWhite,
+          unselectedItemColor: mainWhite,
+          type: BottomNavigationBarType.fixed,
+          items: [
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset('assets/image/icon_home_unselected.svg',
+                  width: 24,
+                  height: 24,
+                  colorFilter: ColorFilter.mode(mainWhite, BlendMode.srcIn)),
+              activeIcon: SvgPicture.asset(
+                  'assets/image/icon_home_selected.svg',
+                  width: 24,
+                  height: 24,
+                  colorFilter: ColorFilter.mode(mainWhite, BlendMode.srcIn)),
+              label: 'home',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                  'assets/image/icon_recommend_unselected.svg',
+                  width: 24,
+                  height: 24,
+                  colorFilter: ColorFilter.mode(mainWhite, BlendMode.srcIn)),
+              activeIcon: SvgPicture.asset(
+                  'assets/image/icon_recommend_selected.svg',
+                  width: 24,
+                  height: 24,
+                  colorFilter: ColorFilter.mode(mainWhite, BlendMode.srcIn)),
+              label: 'recommend',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                  'assets/image/icon_register_unselected.svg',
+                  width: 24,
+                  height: 24,
+                  colorFilter: ColorFilter.mode(mainWhite, BlendMode.srcIn)),
+              label: 'search',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset('assets/image/icon_archive_unselected.svg',
+                  width: 24,
+                  height: 24,
+                  colorFilter: ColorFilter.mode(mainWhite, BlendMode.srcIn)),
+              activeIcon: SvgPicture.asset(
+                  'assets/image/icon_archive_selected.svg',
+                  width: 24,
+                  height: 24,
+                  colorFilter: ColorFilter.mode(mainWhite, BlendMode.srcIn)),
+              label: 'recommend',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset('assets/image/icon_mypage_unselected.svg',
+                  width: 24,
+                  height: 24,
+                  colorFilter: ColorFilter.mode(mainWhite, BlendMode.srcIn)),
+              activeIcon: SvgPicture.asset(
+                  'assets/image/icon_mypage_selected.svg',
+                  width: 24,
+                  height: 24,
+                  colorFilter: ColorFilter.mode(mainWhite, BlendMode.srcIn)),
+              label: 'mypage',
+            ),
+          ]),
     );
   }
 }
