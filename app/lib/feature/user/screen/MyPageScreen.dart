2635198f05a977b4e54de6e0a156cf31b3490098
kdh4718 <charlie7276@naver.com>
2025-03-28 16:26:27 +0900

    [Refactor] Move Mypage DummyData to AppDummyData #front
    Former-commit-id: 609243a203d370b2b315c3efa44b286d0f3f5012

--- app/lib/feature/user/screen/MyPageScreen.dart
+++ app/lib/feature/user/screen/MyPageScreen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:jamesboard/feature/user/widget/item/ItemUserGenrePercentInfo.dart';
 import 'package:jamesboard/theme/Colors.dart';
+import 'package:jamesboard/util/dummy/AppDummyData.dart';
 
 import '../../../util/CommonUtils.dart';
 import '../../../widget/image/ImageCommonMyPageGameCard.dart';
@@ -130,84 +131,6 @@ class _MyPageScreenState extends State<MyPageScreen>
 
   // 임무 보고
   Widget _buildTabContent1() {
-    var images = [
-      {
-        'id': '1',
-        'img':
-            'https://cf.geekdo-images.com/rpwCZAjYLD940NWwP3SRoA__thumb/img/YT6svCVsWqLrDitcMEtyazVktbQ=/fit-in/200x150/filters:strip_icc()/pic4718279.jpg'
-      },
-      {
-        'id': '2',
-        'img':
-            'https://cf.geekdo-images.com/o9-sNXmFS_TLAb7ZlZ4dRA__thumb/img/22MSUC0-ZWgwzhi_VKIbENJik1w=/fit-in/200x150/filters:strip_icc()/pic3211873.jpg'
-      },
-      {
-        'id': '3',
-        'img':
-            'https://cf.geekdo-images.com/FfguJeknahk88vKT7C3JLA__thumb/img/cpf23VxElZxuYaIGcgrjPn80sZY=/fit-in/200x150/filters:strip_icc()/pic7376875.jpg'
-      },
-      {
-        'id': '4',
-        'img':
-            'https://cf.geekdo-images.com/8SADtu_4zBH_UJrCo935Iw__thumb/img/vwTEQOWA3Mw__ztkTMulOgJ82Pw=/fit-in/200x150/filters:strip_icc()/pic6348964.jpg'
-      },
-      {
-        'id': '5',
-        'img':
-            'https://cf.geekdo-images.com/k7lG683LBZdvFyS-FH-MpA__thumb/img/6KTtiknxxGwd0ARKrlsdoXFtHfI=/fit-in/200x150/filters:strip_icc()/pic6746812.png'
-      },
-      {
-        'id': '6',
-        'img':
-            'https://cf.geekdo-images.com/PyUol9QxBnZQCJqZI6bmSA__thumb/img/virV2Bm82Dql7gh-LZScBwqByik=/fit-in/200x150/filters:strip_icc()/pic8632666.png'
-      },
-      {
-        'id': '7',
-        'img':
-            'https://cf.geekdo-images.com/eJx8hRJ6-86C2VrhECwEPA__thumb/img/J1PlqFqtN-hxuXM4eEi-ekANPP4=/fit-in/200x150/filters:strip_icc()/pic1000553.jpg'
-      },
-      {
-        'id': '8',
-        'img':
-            'https://cf.geekdo-images.com/nMitZr9Lu4Ux7LLqBy7Z8A__thumb/img/mLHWOFfdxrWljImkFZ2ulIhHt7A=/fit-in/200x150/filters:strip_icc()/pic6875169.png'
-      },
-      {
-        'id': '9',
-        'img':
-            'https://cf.geekdo-images.com/soAzNVWglCdVBacNjoCTJw__thumb/img/0UPEyOaSFyqEH1ikrSig_218RQ0=/fit-in/200x150/filters:strip_icc()/pic2338267.jpg'
-      },
-      {
-        'id': '10',
-        'img':
-            'https://cf.geekdo-images.com/wX4cTWNjTG7XiGHGgSpiiw__thumb/img/vU6kaL3f06BaVfmwUV91trrvDng=/fit-in/200x150/filters:strip_icc()/pic8630033.png'
-      },
-      {
-        'id': '11',
-        'img':
-            'https://cf.geekdo-images.com/iif2Nv17Vhw8puN2bATaaw__thumb/img/GtXKwk8tJkmHtUvWPT2FIabXh0w=/fit-in/200x150/filters:strip_icc()/pic7794273.png'
-      },
-      {
-        'id': '12',
-        'img':
-            'https://cf.geekdo-images.com/o23NBqu_LBRAtRaVoOBBhQ__thumb/img/VEXGKhNHkULY2hJ41r4Agob5RvE=/fit-in/200x150/filters:strip_icc()/pic403442.jpg'
-      },
-      {
-        'id': '13',
-        'img':
-            'https://cf.geekdo-images.com/AL5D-dXabY-Lk3PqIFk_0g__thumb/img/Q-76aN92M3OveMp26t7b0i7V-i4=/fit-in/200x150/filters:strip_icc()/pic4597095.jpg'
-      },
-      {
-        'id': '14',
-        'img':
-            'https://cf.geekdo-images.com/3qg1xTP7ZZiu8OolGBYJ1w__thumb/img/2LGbUy0Hen-8-8bQ-ZfwHjNnCtU=/fit-in/200x150/filters:strip_icc()/pic1766273.jpg'
-      },
-      {
-        'id': '15',
-        'img':
-            'https://cf.geekdo-images.com/ajU3xzGDUKihjNdvjCR1Hw__thumb/img/pXR57sUMEJ-GJydf-yyAu0ZLgZw=/fit-in/200x150/filters:strip_icc()/pic3328391.jpg'
-      },
-    ];
-
     void handleImageTap(String id) {
       print('클릭한 이미지 ID: $id');
       // 여기에 원하는 동작 추가
@@ -215,7 +138,7 @@ class _MyPageScreenState extends State<MyPageScreen>
 
     return Expanded(
       child: ImageCommonMyPageGameCard(
-        images: images,
+        images: AppDummyData.missionReportimages,
         onTap: handleImageTap,
       ),
     );
@@ -223,98 +146,6 @@ class _MyPageScreenState extends State<MyPageScreen>
 
   // 임무 통계
   Widget _buildTabContent2() {
-    var genres = [
-      {'genre': '전략', 'percent': 25, 'count': 33, 'color': CommonUtils.getGenreColor('전략')},
-      {'genre': '파티', 'percent': 22, 'count': 100, 'color': CommonUtils.getGenreColor('파티')},
-      {'genre': '추리', 'percent': 19, 'count': 95, 'color': CommonUtils.getGenreColor('추리')},
-      {'genre': '경제', 'percent': 17.5, 'count': 80, 'color': CommonUtils.getGenreColor('경제')},
-      {'genre': '모험', 'percent': 16.5, 'count': 70, 'color': CommonUtils.getGenreColor('모험')},
-      {'genre': '전쟁', 'percent': 0, 'count': 0, 'color': CommonUtils.getGenreColor('전쟁')},
-    ];
-
-    List<ChartData> chartData = genres.map((genre) {
-      return ChartData(
-        genre['genre'] as String,
-        (genre['percent'] as num).toDouble(),
-        genre['color'] as Color,
-        genre['count'] as int, // count 추가
-      );
-    }).toList();
-
-
-    var gameData = [
-      {
-        'id': '1',
-        'img':
-            'https://cf.geekdo-images.com/rpwCZAjYLD940NWwP3SRoA__thumb/img/YT6svCVsWqLrDitcMEtyazVktbQ=/fit-in/200x150/filters:strip_icc()/pic4718279.jpg',
-        'title': '디 마허',
-        'round': '35',
-      },
-      {
-        'id': '2',
-        'img':
-            'https://cf.geekdo-images.com/o9-sNXmFS_TLAb7ZlZ4dRA__thumb/img/22MSUC0-ZWgwzhi_VKIbENJik1w=/fit-in/200x150/filters:strip_icc()/pic3211873.jpg',
-        'title': '사무라이',
-        'round': '35',
-      },
-      {
-        'id': '3',
-        'img':
-            'https://cf.geekdo-images.com/FfguJeknahk88vKT7C3JLA__thumb/img/cpf23VxElZxuYaIGcgrjPn80sZY=/fit-in/200x150/filters:strip_icc()/pic7376875.jpg',
-        'title': '어콰이어',
-        'round': '35',
-      },
-      {
-        'id': '4',
-        'img':
-            'https://cf.geekdo-images.com/8SADtu_4zBH_UJrCo935Iw__thumb/img/vwTEQOWA3Mw__ztkTMulOgJ82Pw=/fit-in/200x150/filters:strip_icc()/pic6348964.jpg',
-        'title': '보난자',
-        'round': '35',
-      },
-      {
-        'id': '5',
-        'img':
-            'https://cf.geekdo-images.com/k7lG683LBZdvFyS-FH-MpA__thumb/img/6KTtiknxxGwd0ARKrlsdoXFtHfI=/fit-in/200x150/filters:strip_icc()/pic6746812.png',
-        'title': '라',
-        'round': '35',
-      },
-      {
-        'id': '6',
-        'img':
-            'https://cf.geekdo-images.com/PyUol9QxBnZQCJqZI6bmSA__thumb/img/virV2Bm82Dql7gh-LZScBwqByik=/fit-in/200x150/filters:strip_icc()/pic8632666.png',
-        'title': '카탄',
-        'round': '35',
-      },
-      {
-        'id': '7',
-        'img':
-            'https://cf.geekdo-images.com/eJx8hRJ6-86C2VrhECwEPA__thumb/img/J1PlqFqtN-hxuXM4eEi-ekANPP4=/fit-in/200x150/filters:strip_icc()/pic1000553.jpg',
-        'title': '로보랠리',
-        'round': '35',
-      },
-      {
-        'id': '8',
-        'img':
-            'https://cf.geekdo-images.com/nMitZr9Lu4Ux7LLqBy7Z8A__thumb/img/mLHWOFfdxrWljImkFZ2ulIhHt7A=/fit-in/200x150/filters:strip_icc()/pic6875169.png',
-        'title': '디 마허',
-        'round': '35',
-      },
-      {
-        'id': '9',
-        'img':
-            'https://cf.geekdo-images.com/soAzNVWglCdVBacNjoCTJw__thumb/img/0UPEyOaSFyqEH1ikrSig_218RQ0=/fit-in/200x150/filters:strip_icc()/pic2338267.jpg',
-        'title': '멈출 수 없어',
-        'round': '35',
-      },
-      {
-        'id': '10',
-        'img':
-            'https://cf.geekdo-images.com/wX4cTWNjTG7XiGHGgSpiiw__thumb/img/vU6kaL3f06BaVfmwUV91trrvDng=/fit-in/200x150/filters:strip_icc()/pic8630033.png',
-        'title': '티그리스 & 유프라테스',
-        'round': '35',
-      }
-    ];
-
     return SingleChildScrollView(
       child: Padding(
         padding: const EdgeInsets.all(20.0),
@@ -337,8 +168,8 @@ class _MyPageScreenState extends State<MyPageScreen>
                 ),
               ],
             ),
-            ChartUserGenrePercent(chartData: chartData),
-            ItemUserGenrePercentInfo(genres: genres),
+            ChartUserGenrePercent(chartData: AppDummyData.missionStatisticsChartData),
+            ItemUserGenrePercentInfo(genres: AppDummyData.missionStatisticsGenres),
             SizedBox(
               height: 32,
             ),
@@ -359,8 +190,8 @@ class _MyPageScreenState extends State<MyPageScreen>
                       context,
                       MaterialPageRoute(
                         builder: (context) => MyPagePlayTime(
-                          title: "작전 누적 시간 순위",
-                          gameData: gameData,
+                          title: "작전 누적 판수 순위",
+                          gameData: AppDummyData.missionCumulativeGameData,
                         ),
                       ),
                     );
@@ -379,7 +210,7 @@ class _MyPageScreenState extends State<MyPageScreen>
             SizedBox(
               height: 24,
             ),
-            ItemCommonGameRank(gameData: gameData.take(5).toList()),
+            ItemCommonGameRank(gameData: AppDummyData.missionCumulativeGameData.take(5).toList()),
           ],
         ),
       ),
