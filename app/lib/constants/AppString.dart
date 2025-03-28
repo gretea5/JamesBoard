9abd405c74c4e9ea63f69c5f888fdb16233d0c58
박장훈 <gretea4489@gmail.com>
2025-03-28 10:09:19 +0900

    [Feat] 앱 문자열 클래스 추가 #front
    Former-commit-id: a88911c11d1ad66beeb3fa175fecb6c5a76a7d29

---
+++ app/lib/constants/AppString.dart
@@ -0,0 +1,18 @@
+class AppString {
+  // HomePage
+  static const String labelHome = "home";
+  static const String labelRecommend = "recommend";
+  static const String labelSearch = "";
+  static const String labelArchive = "archive";
+  static const String labelMyPage = "mypage";
+
+  static const String missionEditTitle = "임무 보고";
+  static const String missionListTitle = "임무 보고 아카이브";
+
+  //SearchBarCommonTitle
+  static const String searchBarHint = "어떤 보드게임을 찾으시나요?";
+  static const String cancel = "취소";
+
+  //TextFieldCommonGameRound
+  static const String gameRoundHint = "판수를 입력하세요.";
+}
