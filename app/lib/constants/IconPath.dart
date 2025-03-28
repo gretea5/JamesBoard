7c33b9b84c6efd5aab658a7a5936c715a2deff2c
박장훈 <gretea4489@gmail.com>
2025-03-28 10:08:54 +0900

    [Feat] 아이콘 경로 클래스 추가 #front
    Former-commit-id: 6ff5f21d79ed0cbd6d378bae3e30e0840fed872a

---
+++ app/lib/constants/IconPath.dart
@@ -0,0 +1,35 @@
+class IconPath {
+  //HomePage
+  static const String homeUnselected = 'assets/image/icon_home_unselected.svg';
+  static const String homeSelected = 'assets/image/icon_home_selected.svg';
+
+  //HomePage BottomNavigationBar
+  static const String recommendUnselected =
+      'assets/image/icon_recommend_unselected.svg';
+  static const String recommendSelected =
+      'assets/image/icon_recommend_selected.svg';
+
+  static const String registerUnselected =
+      'assets/image/icon_register_unselected.svg';
+
+  static const String archiveUnselected =
+      'assets/image/icon_archive_unselected.svg';
+  static const String archiveSelected =
+      'assets/image/icon_archive_selected.svg';
+
+  static const String myPageUnselected =
+      'assets/image/icon_mypage_unselected.svg';
+  static const String myPageSelected = 'assets/image/icon_mypage_selected.svg';
+
+  //HomeAppBar
+  static const String chat = 'assets/image/icon_chat.svg';
+  static const String search = 'assets/image/icon_search.svg';
+
+  //BottomSheetCommonFilter
+  static const String filterCheck = 'assets/image/icon_filter_check.svg';
+  static const String close = 'assets/image/icon_close.svg';
+
+  //ItemCommonGameRank
+  static const String bangImage = 'assets/image/bang.png';
+  static const String time = 'assets/image/icon_time.svg';
+}
