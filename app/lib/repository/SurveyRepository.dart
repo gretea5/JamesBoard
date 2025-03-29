8dc2be1cd770beb2d2fcdbee328a682441197cc5
hyuun <wotj28@naver.com>
2025-03-29 23:27:59 +0900

    [Feat] Add getTop30BoardGameByGenre & insertUserPreferBoardGameSurvey in SurveyRepository #front
    Former-commit-id: aa4b2a2d28e6a9eca65effa39092478cc6e00230

--- app/lib/repository/SurveyRepository.dart
+++ app/lib/repository/SurveyRepository.dart
@@ -1,6 +1,9 @@
 import 'package:jamesboard/datasource/api/SurveyService.dart';
 import 'package:jamesboard/util/DioProviderUtil.dart';
 
+import '../datasource/model/request/SurveyBoardGameRequest.dart';
+import '../datasource/model/response/SurveyBoardGameResponse.dart';
+
 class SurveyRepository {
   final SurveyService _service;
 
@@ -14,4 +17,13 @@ class SurveyRepository {
   // 유저 선호 게임 조회
   Future<int> checkUserPreferBoardGame(int userId) =>
       _service.checkUserPreferBoardGame(userId);
+
+  // 장르별 TOP 30 보드게임 조회
+  Future<List<SurveyBoardGameResponse>> getTop30BoardGameByGenre(
+          String category) =>
+      _service.getTop30BoardGameByGenre(category);
+
+  // 유저 선호게임 수정(사실상 등록)
+  Future<int> insertUserPreferBoardGameSurvey(SurveyBoardGameRequest request) =>
+      _service.insertUserPreferBoardGameSurvey(request);
 }
