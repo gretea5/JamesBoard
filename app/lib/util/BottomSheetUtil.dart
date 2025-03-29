4dcd5baca8b5a924d9c1fb45db3a161e4704f991
박장훈 <gretea4489@gmail.com>
2025-03-30 05:48:57 +0900

    [Style] BottomSheetBoardGmaeEvalatuion.dart 생성 #front
    Former-commit-id: 5e869882b827f719da8dc89821165dd2ec3c28d8

--- app/lib/util/BottomSheetUtil.dart
+++ app/lib/util/BottomSheetUtil.dart
@@ -4,6 +4,7 @@ import 'package:jamesboard/theme/Colors.dart';
 import '../constants/AppString.dart';
 import '../constants/FontString.dart';
 import '../feature/boardgame/widget/BottomSheetBoardGameDetailDetail.dart';
+import '../feature/boardgame/widget/BottomSheetBoardGameEvaluation.dart';
 import '../feature/boardgame/widget/RatingBarBoardGameDetailReview.dart';
 
 class BottomSheetUtil {
@@ -33,74 +34,12 @@ class BottomSheetUtil {
   }
 
   static void showRatingBottomSheet(BuildContext context) {
-    print("showRatingBottomSheet 호출됨!"); // 디버깅 로그 추가
-
     showModalBottomSheet(
       context: context,
       isScrollControlled: true,
       backgroundColor: Colors.transparent,
       builder: (BuildContext context) {
-        print("BottomSheet builder 실행됨!"); // 디버깅 로그 추가
-        return Container(
-          constraints: BoxConstraints(
-            maxHeight: MediaQuery.of(context).size.height * 0.35,
-          ),
-          decoration: BoxDecoration(
-            color: secondaryBlack,
-            borderRadius: const BorderRadius.vertical(top: Radius.circular(20)),
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Container(
-                margin: EdgeInsets.only(top: 20),
-                child: Text(
-                  "클루",
-                  style: TextStyle(
-                    color: mainWhite,
-                    fontSize: 20,
-                    fontFamily: FontString.pretendardBold,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20),
-              RatingBarBoardGameDetailReview(
-                initialRating: 0,
-                onRatingUpdate: (rating) {
-                  print("사용자 평가: $rating");
-                },
-              ),
-              const SizedBox(height: 20),
-              // 닫기 버튼
-              Container(
-                width: double.infinity,
-                decoration: const BoxDecoration(
-                  border: Border(
-                    top: BorderSide(
-                      color: mainGrey,
-                      width: 1.0,
-                    ),
-                  ),
-                ),
-                child: ElevatedButton(
-                  onPressed: () => Navigator.of(context).pop(),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: secondaryBlack,
-                    padding: const EdgeInsets.symmetric(vertical: 24),
-                  ),
-                  child: Text(
-                    "적용",
-                    style: TextStyle(
-                      color: mainWhite,
-                      fontSize: 20,
-                      fontFamily: FontString.pretendardBold,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        );
+        return BottomSheetBoardGameEvaluation();
       },
     );
   }
