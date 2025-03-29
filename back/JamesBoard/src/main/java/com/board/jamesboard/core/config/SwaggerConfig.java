14959e1c24ba66f2da3168aeac2189388f3f2f73
I'mplant <152685022+KR-ImPlant@users.noreply.github.com>
2025-03-29 20:11:06 +0900

    [Feat] add paragraphStyle by Swagger Config & mypage, auth swagger update
    Former-commit-id: 0e08289e1b7abe66c979a536194b6c819d3473d5

--- back/JamesBoard/src/main/java/com/board/jamesboard/core/config/SwaggerConfig.java
+++ back/JamesBoard/src/main/java/com/board/jamesboard/core/config/SwaggerConfig.java
@@ -1,6 +1,8 @@
 package com.board.jamesboard.core.config;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -30,10 +32,18 @@ public class SwaggerConfig {
         // Security 요청 설정
         SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearer-jwt");
 
+        // 여러줄 형식 추가
+        Map<String, Object> extensions = new HashMap<>();
+        Map<String, Object> swaggerUiConfig = new HashMap<>();
+        swaggerUiConfig.put("paragraphStyle", true);
+
+        extensions.put("x-swagger-ui-config", swaggerUiConfig);
+
         return new OpenAPI()
                 .info(info)
                 .addSecurityItem(securityRequirement)
                 .components(new Components().addSecuritySchemes("bearer-jwt", securityScheme))
+                .extensions(extensions)
                 .servers(List.of(
                         new Server().url("http://localhost:8080"),
                         new Server().url("https://j12d205.p.ssafy.io").description("제임스보드 API 서버")
