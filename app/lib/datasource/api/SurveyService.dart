6147537b2d18c92cd3f4e5a5e8fb170892c6dea9
hyuun <wotj28@naver.com>
2025-03-29 23:24:48 +0900

    [Feat] Add SurveyService.dart #front
    Former-commit-id: 23ba67e0d6f92e58ec29f238923c7e4c0673defd

--- app/lib/datasource/api/SurveyService.dart
+++ app/lib/datasource/api/SurveyService.dart
@@ -1,6 +1,9 @@
 import 'package:dio/dio.dart';
+import 'package:jamesboard/datasource/model/response/SurveyBoardGameResponse.dart';
 import 'package:retrofit/error_logger.dart';
 import 'package:retrofit/http.dart';
+
+import '../model/request/SurveyBoardGameRequest.dart';
 
 part 'SurveyService.g.dart';
 
@@ -12,5 +15,17 @@ abstract class SurveyService {
   @GET("/api/users/{userId}/prefer-games")
   Future<int> checkUserPreferBoardGame(
     @Path("userId") int userId,
+  );
+
+  // 장르별 TOP 30 보드게임 조회
+  @GET("/api/onboard/games")
+  Future<List<SurveyBoardGameResponse>> getTop30BoardGameByGenre(
+    @Query("category") String category,
+  );
+
+  // 유저 선호게임 수정(사실상 등록)
+  @PATCH("/api/onboard/users/{userId}/prefer-games")
+  Future<int> insertUserPreferBoardGameSurvey(
+    @Body() SurveyBoardGameRequest request,
   );
 }
--- app/lib/datasource/api/SurveyService.g.dart
+++ app/lib/datasource/api/SurveyService.g.dart
@@ -46,6 +46,70 @@ class _SurveyService implements SurveyService {
     return _value;
   }
 
+  @override
+  Future<List<SurveyBoardGameResponse>> getTop30BoardGameByGenre(
+    String category,
+  ) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{r'category': category};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<List<SurveyBoardGameResponse>>(
+      Options(method: 'GET', headers: _headers, extra: _extra)
+          .compose(
+            _dio.options,
+            '/api/onboard/games',
+            queryParameters: queryParameters,
+            data: _data,
+          )
+          .copyWith(baseUrl: _combineBaseUrls(_dio.options.baseUrl, baseUrl)),
+    );
+    final _result = await _dio.fetch<List<dynamic>>(_options);
+    late List<SurveyBoardGameResponse> _value;
+    try {
+      _value = _result.data!
+          .map(
+            (dynamic i) =>
+                SurveyBoardGameResponse.fromJson(i as Map<String, dynamic>),
+          )
+          .toList();
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options);
+      rethrow;
+    }
+    return _value;
+  }
+
+  @override
+  Future<int> insertUserPreferBoardGameSurvey(
+    SurveyBoardGameRequest request,
+  ) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    _data.addAll(request.toJson());
+    final _options = _setStreamType<int>(
+      Options(method: 'PATCH', headers: _headers, extra: _extra)
+          .compose(
+            _dio.options,
+            '/api/onboard/users/{userId}/prefer-games',
+            queryParameters: queryParameters,
+            data: _data,
+          )
+          .copyWith(baseUrl: _combineBaseUrls(_dio.options.baseUrl, baseUrl)),
+    );
+    final _result = await _dio.fetch<int>(_options);
+    late int _value;
+    try {
+      _value = _result.data!;
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options);
+      rethrow;
+    }
+    return _value;
+  }
+
   RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
     if (T != dynamic &&
         !(requestOptions.responseType == ResponseType.bytes ||
