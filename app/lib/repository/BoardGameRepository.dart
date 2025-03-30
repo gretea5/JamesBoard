d6d123ad0cf8b995f69cf75bac1684fc62e61abf
박장훈 <gretea4489@gmail.com>
2025-03-30 23:16:15 +0900

    [Feat] BoardGameRepository 추가 #front
    Former-commit-id: b12d52179cf23e90c37a4fa67a2214986804b7f4

---
+++ app/lib/repository/BoardGameRepository.dart
@@ -0,0 +1,30 @@
+import '../datasource/api/BoardGameService.dart';
+import '../datasource/model/response/BoardGameResponse.dart';
+import '../util/DioProviderUtil.dart';
+
+class BoardGameRepository {
+  final BoardGameService _service;
+
+  BoardGameRepository._(this._service);
+
+  factory BoardGameRepository.create() {
+    final dio = DioProviderUtil.dio;
+
+    final service = BoardGameService(dio);
+    return BoardGameRepository._(service);
+  }
+
+  // 추천 보드게임 리스트를 반환하는 메서드
+  Future<List<BoardGameResponse>> getRecommendedGames({int limit = 10}) =>
+      _service.getRecommendedGames(limit: limit);
+
+  Future<List<BoardGameResponse>> getBoardGames(
+      Map<String, dynamic> queryParameters) {
+    return _service.getBoardGames(
+      difficulty: queryParameters['difficulty'] ?? null,
+      minPlayers: queryParameters['minPlayers'] ?? null,
+      category: queryParameters['category'] ?? null,
+      boardgameName: queryParameters['boardgameName'] ?? null,
+    );
+  }
+}
