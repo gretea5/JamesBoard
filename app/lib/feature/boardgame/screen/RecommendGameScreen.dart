cfe69cbfabf100d80bd688baaea789b099c0010b
박장훈 <gretea4489@gmail.com>
2025-03-30 23:17:13 +0900

    [Refactor] RecommendGameScreen 더미데이터 옮기기 #front
    Former-commit-id: 003dd71a4aa5112a17e5089d069e9bceb05887bc

--- app/lib/feature/boardgame/screen/RecommendGameScreen.dart
+++ app/lib/feature/boardgame/screen/RecommendGameScreen.dart
@@ -1,86 +1,73 @@
 import 'package:flutter/material.dart';
-
+import 'package:jamesboard/datasource/api/BoardGameService.dart';
+import 'package:jamesboard/theme/Colors.dart';
+import 'package:jamesboard/util/DioProviderUtil.dart';
+import 'package:provider/provider.dart';
+import '../../../repository/BoardGameRepository.dart';
+import '../viewmodel/BoardGameViewModel.dart';
 import '../widget/ItemRecommendBoardGameInfo.dart';
 import 'BoardGameDetailScreen.dart';
 
 class RecommendGameScreen extends StatelessWidget {
-  final List<Map<String, dynamic>> gameList = [
-    {
-      'gameId': 1,
-      'imageUrl':
-          'https://cf.geekdo-images.com/x3zxjr-Vw5iU4yDPg70Jgw__original/img/FpyxH41Y6_ROoePAilPNEhXnzO8=/0x0/filters:format(jpeg)/pic3490053.jpg',
-      'gameName': '클루',
-      'gameCategory': '추리',
-      'gameTheme': '모험',
-      'gameMinPlayer': 2,
-      'gameMaxPlayer': 4,
-      'gameDifficulty': 1,
-      'gamePlayTime': 60,
-      'gameDescription':
-          '클루에 대한 설명입니다. 클루에 대한 설명입니다. 클루에 대한 설명입니다. 클루에 대한 설명입니다. 클루에 대한 설명입니다.클루에 대한 설명입니다.클루에 대한 ...',
-    },
-    {
-      'gameId': 2,
-      'imageUrl':
-          'https://cf.geekdo-images.com/7SrPNGBKg9IIsP4UQpOi8g__original/img/GKueTbkCk2Ramf6ai8mDj-BP6cI=/0x0/filters:format(jpeg)/pic4325841.jpg',
-      'gameName': '부루마블',
-      'gameCategory': '파티',
-      'gameTheme': '추리',
-      'gameMinPlayer': 3,
-      'gameMaxPlayer': 6,
-      'gameDifficulty': 0,
-      'gamePlayTime': 45,
-      'gameDescription':
-          '부루마블에 대한 설명입니다. 부루마블에 대한 설명입니다. 부루마블에 대한 설명입니다. 부루마블에 대한 설명입니다. 부루마블에 대한 설명입니다.클루에 대한 설명입니다.클루에 대한 ...',
-    },
-    {
-      'gameId': 2,
-      'imageUrl':
-          'https://cf.geekdo-images.com/5CFwjd8zTcGYVUnkXh04hw__original/img/N8btACZrnEYK1amBNk26VBdcGwc=/0x0/filters:format(jpeg)/pic1176894.jpg',
-      'gameName': '클루',
-      'gameCategory': '파티',
-      'gameTheme': '추리',
-      'gameMinPlayer': 3,
-      'gameMaxPlayer': 6,
-      'gameDifficulty': 0,
-      'gamePlayTime': 45,
-      'gameDescription':
-          '클루에 대한 설명입니다. 클루에 대한 설명입니다. 클루에 대한 설명입니다. 클루에 대한 설명입니다. 클루에 대한 설명입니다.클루에 대한 설명입니다.클루에 대한 ...',
-    },
-    // 추가 게임 데이터
-  ];
-
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      padding: EdgeInsets.all(16),
-      itemCount: gameList.length,
-      itemBuilder: (context, index) {
-        final game = gameList[index];
-        return GestureDetector(
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => BoardGameDetailScreen()),
-            );
-          },
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 20),
-            child: ItemRecommendBoardGameInfo(
-              gameId: game['gameId'],
-              imageUrl: game['imageUrl'],
-              gameName: game['gameName'],
-              gameCategory: game['gameCategory'],
-              gameTheme: game['gameTheme'],
-              gameMinPlayer: game['gameMinPlayer'],
-              gameMaxPlayer: game['gameMaxPlayer'],
-              gameDifficulty: game['gameDifficulty'],
-              gamePlayTime: game['gamePlayTime'],
-              gameDescription: game['gameDescription'],
-            ),
-          ),
-        );
-      },
+    return ChangeNotifierProvider(
+      create: (_) => BoardGameViewModel(BoardGameRepository.create())
+        ..getRecommendedGames(),
+      child: Consumer<BoardGameViewModel>(
+        builder: (context, viewModel, child) {
+          if (viewModel.isLoading) {
+            return Center(child: CircularProgressIndicator());
+          } else if (viewModel.errorMessage != null) {
+            return Center(
+                child: Text(
+              viewModel.errorMessage!,
+              style: TextStyle(
+                color: mainWhite,
+              ),
+            ));
+          } else if (viewModel.recommendedGames.isEmpty) {
+            return Center(
+                child: Text(
+              '추천 보드게임이 없습니다.',
+              style: TextStyle(
+                color: mainWhite,
+              ),
+            ));
+          }
+          return ListView.builder(
+            padding: EdgeInsets.all(16),
+            itemCount: viewModel.recommendedGames.length,
+            itemBuilder: (context, index) {
+              final game = viewModel.recommendedGames[index];
+              return GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => BoardGameDetailScreen()),
+                  );
+                },
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 24, horizontal: 20),
+                  child: ItemRecommendBoardGameInfo(
+                    gameId: game.gameId,
+                    imageUrl: game.gameImage,
+                    gameName: game.gameTitle,
+                    gameCategory: game.gameCategory,
+                    gameMinPlayer: game.minPlayer,
+                    gameMaxPlayer: game.maxPlayer,
+                    gameDifficulty: game.difficulty,
+                    gamePlayTime: game.playTime,
+                    gameDescription: game.gameDescription,
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
