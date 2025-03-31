1603d2c6f1dad510568ab3b552342609573fc584
박장훈 <gretea4489@gmail.com>
2025-03-31 23:15:54 +0900

    [Feat] BoardGameViewModel에서 데이터가 있을 경우, 조회하지 않도록 변경 #front
    Former-commit-id: b5f2053ca8848465c3bfcaa3aa7648ae90aa1fc7

--- app/lib/feature/boardgame/viewmodel/BoardGameViewModel.dart
+++ app/lib/feature/boardgame/viewmodel/BoardGameViewModel.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/foundation.dart';
+import 'package:jamesboard/datasource/model/response/BoardGameRecommendResponse.dart';
 
 import 'package:jamesboard/datasource/model/response/BoardGameResponse.dart';
 import 'package:jamesboard/datasource/model/response/BoardGameTopResponse.dart';
@@ -9,8 +10,8 @@ import '../../../repository/BoardGameRepository.dart';
 class BoardGameViewModel extends ChangeNotifier {
   final BoardGameRepository _repository;
 
-  List<BoardGameResponse> _recommendedGames = [];
-  List<BoardGameResponse> get recommendedGames => _recommendedGames;
+  List<BoardGameRecommendResponse> _recommendedGames = [];
+  List<BoardGameRecommendResponse> get recommendedGames => _recommendedGames;
 
   List<BoardGameResponse> _games = [];
   List<BoardGameResponse> get games => _games;
@@ -31,6 +32,11 @@ class BoardGameViewModel extends ChangeNotifier {
     _errorMessage = null;
     notifyListeners();
 
+    if (_recommendedGames.isNotEmpty) {
+      _isLoading = false;
+      notifyListeners();
+    }
+
     try {
       _recommendedGames = await _repository.getRecommendedGames(limit: limit);
     } catch (e) {
@@ -47,6 +53,11 @@ class BoardGameViewModel extends ChangeNotifier {
     _errorMessage = null;
     notifyListeners();
 
+    if (_games.isNotEmpty) {
+      _isLoading = false;
+      notifyListeners();
+    }
+
     try {
       _games = await _repository.getBoardGames(queryParameters);
     } catch (e) {
@@ -61,6 +72,11 @@ class BoardGameViewModel extends ChangeNotifier {
     _isLoading = true;
     _errorMessage = null;
     notifyListeners();
+
+    if (_topGames.isNotEmpty) {
+      _isLoading = false;
+      notifyListeners();
+    }
 
     try {
       _topGames = await _repository.getTopGames(queryParameters);
