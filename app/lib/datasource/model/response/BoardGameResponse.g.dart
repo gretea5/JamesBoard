515dad5ff327c841e8835cb9a23a09f2eac41df5
박장훈 <gretea4489@gmail.com>
2025-03-31 22:27:12 +0900

    [Refactor] BoardGameResponse 속성 변경 #front
    Former-commit-id: ccee09ac4e6cff4e8febd5fe97bd90d0b74680cd

--- app/lib/datasource/model/response/BoardGameResponse.g.dart
+++ app/lib/datasource/model/response/BoardGameResponse.g.dart
@@ -6,19 +6,22 @@ part of 'BoardGameResponse.dart';
 // JsonSerializableGenerator
 // ***************************************************************************
 
-BoardGameResponse _$BoardGameResponseFromJson(Map<String, dynamic> json) {
-  return BoardGameResponse(
-    gameId: json['gameId'] as int,
-    gameTitle: json['gameTitle'] as String,
-    gameImage: json['gameImage'] as String,
-    gameCategory: json['gameCategory'] as String,
-    minPlayer: json['minPlayer'] as int,
-    maxPlayer: json['maxPlayer'] as int,
-    difficulty: json['difficulty'] as int,
-    playTime: json['playTime'] as int,
-    gameDescription: json['gameDescription'] as String,
-  );
-}
+BoardGameResponse _$BoardGameResponseFromJson(Map<String, dynamic> json) =>
+    BoardGameResponse(
+      gameId: json['gameId'] as int,
+      gameTitle: json['gameTitle'] as String,
+      gameImage: json['gameImage'] as String,
+      gameCategory: (json['gameCategory'] as List<dynamic>)
+          .map((e) => e as String)
+          .toList(),
+      gameTheme:
+          (json['gameTheme'] as List<dynamic>).map((e) => e as String).toList(),
+      minPlayer: json['minPlayer'] as int,
+      maxPlayer: json['maxPlayer'] as int,
+      difficulty: json['difficulty'] as int,
+      playTime: json['playTime'] as int,
+      gameDescription: json['gameDescription'] as String,
+    );
 
 Map<String, dynamic> _$BoardGameResponseToJson(BoardGameResponse instance) =>
     <String, dynamic>{
@@ -26,6 +29,7 @@ Map<String, dynamic> _$BoardGameResponseToJson(BoardGameResponse instance) =>
       'gameTitle': instance.gameTitle,
       'gameImage': instance.gameImage,
       'gameCategory': instance.gameCategory,
+      'gameTheme': instance.gameTheme,
       'minPlayer': instance.minPlayer,
       'maxPlayer': instance.maxPlayer,
       'difficulty': instance.difficulty,
