3df9023ae30e7ad57155a50ae95503b2d7be0eba
hyuun <wotj28@naver.com>
2025-04-01 14:24:34 +0900

    [Style] Fix SelectBoxRegisterMissionBoardGame.dart #front
    Former-commit-id: 5bfa24fe92394210bd42c5f3c40a56e80fa552fa [formerly 11a861ab3ba6f56ced34a54d56efe898ae0e945c [formerly 32cbaf12be44c5b9eaccdf565cedceb6a8e7f083] [formerly 5bfa24fe92394210bd42c5f3c40a56e80fa552fa [formerly 053043142587932e346b6e30c6b29cc0984100d6]]]
    Former-commit-id: 11a861ab3ba6f56ced34a54d56efe898ae0e945c [formerly 32cbaf12be44c5b9eaccdf565cedceb6a8e7f083]
    Former-commit-id: 11a861ab3ba6f56ced34a54d56efe898ae0e945c
    Former-commit-id: e7c74548870e2d0cf3b11273bada45e401be4ff2

--- app/lib/feature/mission/widget/SelectBoxRegisterMissionBoardGame.dart
+++ app/lib/feature/mission/widget/SelectBoxRegisterMissionBoardGame.dart
@@ -1,58 +1,44 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:jamesboard/constants/AppString.dart';
 import 'package:jamesboard/constants/FontString.dart';
 import 'package:jamesboard/constants/IconPath.dart';
 import 'package:jamesboard/theme/Colors.dart';
 
-class SelectBoxRegisterMissionBoardGame extends StatefulWidget {
-  const SelectBoxRegisterMissionBoardGame({super.key});
+class SelectBoxRegisterMissionBoardGame extends StatelessWidget {
+  final String? selectedGame;
 
-  @override
-  State<SelectBoxRegisterMissionBoardGame> createState() =>
-      _SelectBoxRegisterMissionBoardGameState();
-}
-
-class _SelectBoxRegisterMissionBoardGameState
-    extends State<SelectBoxRegisterMissionBoardGame> {
-  String? _selectedGame;
-
-  // 임시로 상태 변경하는 메서드
-  void _handleSelectGame() async {
-    String selectedGame =
-        await Future.delayed(const Duration(milliseconds: 500), () => '클루');
-
-    setState(() {
-      _selectedGame = selectedGame;
-    });
-  }
+  const SelectBoxRegisterMissionBoardGame({
+    super.key,
+    required this.selectedGame,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: _handleSelectGame,
-      child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-        decoration: BoxDecoration(
-            color: secondaryBlack, borderRadius: BorderRadius.circular(10)),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              _selectedGame ?? AppString.selectBoardGame,
-              style: TextStyle(
-                  color: _selectedGame != null ? mainWhite : mainGrey,
-                  fontSize: 16,
-                  fontFamily: FontString.pretendardMedium),
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+      decoration: BoxDecoration(
+        color: secondaryBlack,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            selectedGame ?? AppString.selectBoardGame,
+            style: TextStyle(
+              color: selectedGame != null ? mainWhite : mainGrey,
+              fontSize: 16,
+              fontFamily: FontString.pretendardMedium,
             ),
-            SvgPicture.asset(
-              IconPath.arrowRight,
-              width: 24,
-              height: 24,
-              color: mainGrey,
-            ),
-          ],
-        ),
+          ),
+          SvgPicture.asset(
+            IconPath.arrowRight,
+            width: 24,
+            height: 24,
+            color: mainGrey,
+          ),
+        ],
       ),
     );
   }
