757a28948bf2af1493a1e401256386a7da074c9b
hyuun <wotj28@naver.com>
2025-04-01 11:10:29 +0900

    [Feat] Add 아카이브 상세 조회 #front
    Former-commit-id: d7f965628035380a5155aa2ed0bc8e97cce4e02f [formerly 4f023b46fb1ceb3ce65f0dca34e12f48bdc8e780 [formerly 9bad2b99205e5ef00e15d56e3e21fd2cb29c0d97] [formerly d7f965628035380a5155aa2ed0bc8e97cce4e02f [formerly 9484e47d6b9cd33bfb90be6e3f2b44a509b9db37]]]
    Former-commit-id: 4f023b46fb1ceb3ce65f0dca34e12f48bdc8e780 [formerly 9bad2b99205e5ef00e15d56e3e21fd2cb29c0d97]
    Former-commit-id: 4f023b46fb1ceb3ce65f0dca34e12f48bdc8e780
    Former-commit-id: a7ef7a45c642e7407eb1da0e1494ea923b610d78

--- app/lib/feature/mission/viewmodel/MissionViewModel.dart
+++ app/lib/feature/mission/viewmodel/MissionViewModel.dart
@@ -5,12 +5,19 @@ import 'package:jamesboard/main.dart';
 import 'package:jamesboard/repository/ArchiveRepository.dart';
 import 'package:jamesboard/repository/LoginRepository.dart';
 
+import '../../../datasource/model/response/ArchiveDetailResponse.dart';
+
 class MissionViewModel extends ChangeNotifier {
   final ArchiveRepository _archiveRepository;
   final LoginRepository _loginRepository;
 
   List<ArchiveListResponse> _archives = [];
   List<ArchiveListResponse> get archives => _archives;
+
+  bool isLoading = false;
+  bool hasError = false;
+
+  ArchiveDetailResponse? archiveDetailResponse;
 
   MissionViewModel(
     this._archiveRepository,
@@ -30,5 +37,30 @@ class MissionViewModel extends ChangeNotifier {
     } catch (e) {
       logger.e('아카이브 전체 조회 실패 : $e');
     }
+  }
+
+  // 아카이브 상세 조회
+  Future<void> getArchiveById(int archiveId) async {
+    isLoading = true;
+    hasError = false;
+    archiveDetailResponse = null;
+    notifyListeners();
+
+    try {
+      archiveDetailResponse =
+          await _archiveRepository.getArchiveById(archiveId);
+    } on DioException catch (e) {
+      if (e.response?.statusCode == 401) {
+        logger.e('401 에러 발생. 로그아웃 처리.');
+        await _loginRepository.logout();
+      }
+      hasError = true;
+    } catch (e) {
+      logger.e('아카이브 상세 조회 실패 : $e');
+      hasError = true;
+    }
+
+    isLoading = false;
+    notifyListeners();
   }
 }
