eecd8d787ab4886eb6f1c87a60d2a166e65de82f
hyuun <wotj28@naver.com>
2025-04-01 12:49:07 +0900

    [Fix] 아카이브를 등록한 user의 userId, 플레이 판 수 추가 #front
    Former-commit-id: bc6dddfae4d5f86e1c16b63b3d0f320b6644177f [formerly 6e2da4491ca18fba00362aa9735cd141bd7b7f0e [formerly 6dec49cfe31fa9b9ecace410e1d4360f14af2c82] [formerly bc6dddfae4d5f86e1c16b63b3d0f320b6644177f [formerly cc67ac88a5729b7ca0c9f8296ada052a2136a076]]]
    Former-commit-id: 6e2da4491ca18fba00362aa9735cd141bd7b7f0e [formerly 6dec49cfe31fa9b9ecace410e1d4360f14af2c82]
    Former-commit-id: 6e2da4491ca18fba00362aa9735cd141bd7b7f0e
    Former-commit-id: f28c54b94e7ab9c8b4a966bf7d57fa3bbfe2d7fb

--- app/lib/datasource/model/response/ArchiveDetailResponse.dart
+++ app/lib/datasource/model/response/ArchiveDetailResponse.dart
@@ -5,20 +5,24 @@ part 'ArchiveDetailResponse.g.dart';
 @JsonSerializable()
 class ArchiveDetailResponse {
   final int archiveId;
+  final int userId;
   final String userNickName;
   final String userProfile;
   final String archiveContent;
   final String gameTitle;
   final int archiveGamePlayTime;
+  final int archiveGamePlayCount;
   final List<String> archiveImageList;
 
   ArchiveDetailResponse({
     required this.archiveId,
+    required this.userId,
     required this.userNickName,
     required this.userProfile,
     required this.archiveContent,
     required this.gameTitle,
     required this.archiveGamePlayTime,
+    required this.archiveGamePlayCount,
     required this.archiveImageList,
   });
 
