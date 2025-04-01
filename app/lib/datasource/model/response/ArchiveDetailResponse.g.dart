496c438ca921746b14c2bfc92aef2b71b3fefdd9
hyuun <wotj28@naver.com>
2025-04-01 12:39:52 +0900

    [Fix] ArchiveDetailResponse.dart에서 아카이브를 등록한 userId, 플레이 판 수 추가 #front
    Former-commit-id: 691ad25780a56e886ad970d084a661224460ad3a [formerly 214e77ccd077b8965dfeacf74a72d98f3d19316b [formerly b36c88e2be1152da5e397a8a9527c1e4b71abae9] [formerly 691ad25780a56e886ad970d084a661224460ad3a [formerly 014c3656c8628b58af23c0295008ae16f8855fbc]]]
    Former-commit-id: 214e77ccd077b8965dfeacf74a72d98f3d19316b [formerly b36c88e2be1152da5e397a8a9527c1e4b71abae9]
    Former-commit-id: 214e77ccd077b8965dfeacf74a72d98f3d19316b
    Former-commit-id: fba4b9899205a72d6bc67de709363f57caacd669

--- app/lib/datasource/model/response/ArchiveDetailResponse.g.dart
+++ app/lib/datasource/model/response/ArchiveDetailResponse.g.dart
@@ -10,11 +10,13 @@ ArchiveDetailResponse _$ArchiveDetailResponseFromJson(
         Map<String, dynamic> json) =>
     ArchiveDetailResponse(
       archiveId: (json['archiveId'] as num).toInt(),
+      userId: (json['userId'] as num).toInt(),
       userNickName: json['userNickName'] as String,
       userProfile: json['userProfile'] as String,
       archiveContent: json['archiveContent'] as String,
       gameTitle: json['gameTitle'] as String,
       archiveGamePlayTime: (json['archiveGamePlayTime'] as num).toInt(),
+      archiveGamePlayCount: (json['archiveGamePlayCount'] as num).toInt(),
       archiveImageList: (json['archiveImageList'] as List<dynamic>)
           .map((e) => e as String)
           .toList(),
@@ -24,10 +26,12 @@ Map<String, dynamic> _$ArchiveDetailResponseToJson(
         ArchiveDetailResponse instance) =>
     <String, dynamic>{
       'archiveId': instance.archiveId,
+      'userId': instance.userId,
       'userNickName': instance.userNickName,
       'userProfile': instance.userProfile,
       'archiveContent': instance.archiveContent,
       'gameTitle': instance.gameTitle,
       'archiveGamePlayTime': instance.archiveGamePlayTime,
+      'archiveGamePlayCount': instance.archiveGamePlayCount,
       'archiveImageList': instance.archiveImageList,
     };
