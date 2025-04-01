f54d4f9a62a38c2ef1b758bd90975b630bead16d
hyuun <wotj28@naver.com>
2025-04-01 10:25:08 +0900

    [Feat] MissionViewModel 연결 #front
    Former-commit-id: ee06c117c1e38a4877900c601ab2fea9f8b2b990 [formerly ef8579187ae8ef472dd1d9875d1a78b0df77c8ae [formerly 4776ebd69bd61514dd643e3ca9c6197c99226e77] [formerly ee06c117c1e38a4877900c601ab2fea9f8b2b990 [formerly 7a41fdf7c30ecd3e8adf3c527575a4a08f65d3ce]]]
    Former-commit-id: ef8579187ae8ef472dd1d9875d1a78b0df77c8ae [formerly 4776ebd69bd61514dd643e3ca9c6197c99226e77]
    Former-commit-id: ef8579187ae8ef472dd1d9875d1a78b0df77c8ae
    Former-commit-id: 47a376ce69ec119b8ed95962e8b3ea68c1063391

--- app/lib/feature/mission/screen/MissionListScreen.dart
+++ app/lib/feature/mission/screen/MissionListScreen.dart
@@ -1,28 +1,34 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:jamesboard/feature/mission/widget/ImageItemMissionList.dart';
-import 'package:jamesboard/theme/Colors.dart';
-import 'package:jamesboard/util/dummy/AppDummyData.dart';
+import 'package:provider/provider.dart';
 
-class MissionListScreen extends StatelessWidget {
+import '../viewmodel/MissionViewModel.dart';
+import '../widget/ImageItemMissionList.dart';
+
+class MissionListScreen extends StatefulWidget {
   final String title;
 
-  MissionListScreen({super.key, required this.title});
+  const MissionListScreen({super.key, required this.title});
 
   @override
+  State<MissionListScreen> createState() => _MissionListScreenState();
+}
+
+class _MissionListScreenState extends State<MissionListScreen> {
+  @override
   Widget build(BuildContext context) {
+    final archives = context.watch<MissionViewModel>().archives;
+
     return Padding(
       padding: const EdgeInsets.only(top: 4.0),
       child: GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 3,
           crossAxisSpacing: 2.0,
           mainAxisSpacing: 2.0,
         ),
-        itemCount: AppDummyData.imageAssets.length,
+        itemCount: archives.length,
         itemBuilder: (context, index) {
-          return ImageItemMissionList(
-              imageUrl: AppDummyData.imageAssets[index]);
+          return ImageItemMissionList(imageUrl: archives[index].archiveImage);
         },
       ),
     );
