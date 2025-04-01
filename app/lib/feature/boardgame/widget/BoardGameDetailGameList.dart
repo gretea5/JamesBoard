cceaf789e8b10b4fbd2ca0e492525a102983f9d0
박장훈 <gretea4489@gmail.com>
2025-04-01 23:48:46 +0900

    [Feat] BoardGameDetailGameList 생성 #front
    Former-commit-id: 517b181267a6c7e93a720fbe90c9afc5147b7e76 [formerly 4710cba3e327215a66ef2dbe00c0aef1d4055962 [formerly 11ee95040e7fc1578c589ee735b32455472d7c67] [formerly 517b181267a6c7e93a720fbe90c9afc5147b7e76 [formerly be179b0db580cb07e7b1c3973d3be595ea497a66]]]
    Former-commit-id: 4710cba3e327215a66ef2dbe00c0aef1d4055962 [formerly 11ee95040e7fc1578c589ee735b32455472d7c67]
    Former-commit-id: 4710cba3e327215a66ef2dbe00c0aef1d4055962
    Former-commit-id: abcfcfbefc6c9a74b9cd37815374b9e762b25680

---
+++ app/lib/feature/boardgame/widget/BoardGameDetailGameList.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:jamesboard/util/view/KeepAliveView.dart';
+import 'package:provider/provider.dart';
+
+import '../../../theme/Colors.dart';
+import '../../../widget/image/ImageCommonGameCard.dart';
+import '../viewmodel/BoardGameViewModel.dart';
+
+class BoardGameDetailGameList extends StatefulWidget {
+  final String category;
+  const BoardGameDetailGameList({
+    super.key,
+    required this.category,
+  });
+
+  @override
+  State<BoardGameDetailGameList> createState() =>
+      _BoardGameDetailGameListState();
+}
+
+class _BoardGameDetailGameListState extends State<BoardGameDetailGameList> {
+  late BoardGameViewModel viewModel;
+
+  @override
+  void initState() {
+    super.initState();
+    viewModel = Provider.of<BoardGameViewModel>(context, listen: false);
+    viewModel.getBoardGames({"category": widget.category});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider.value(
+      value: viewModel,
+      child: Consumer<BoardGameViewModel>(builder: (context, viewModel, child) {
+        if (viewModel.isLoading) {
+          return Center(
+            child: CircularProgressIndicator(color: mainGold),
+          );
+        }
+
+        final games = viewModel.games;
+
+        return Container(
+          margin: EdgeInsets.only(
+            top: 24,
+            left: 20,
+            right: 20,
+            bottom: 20,
+          ),
+          child: GridView.builder(
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3, // 한 줄에 3개
+              crossAxisSpacing: 12,
+              mainAxisSpacing: 12,
+              childAspectRatio: 3 / 4,
+            ),
+            itemCount: games.length < 30 ? games.length : 30,
+            itemBuilder: (context, index) {
+              return KeepAliveView(
+                child: ImageCommonGameCard(imageUrl: games[index].gameImage),
+              );
+            },
+          ),
+        );
+      }),
+    );
+  }
+}
