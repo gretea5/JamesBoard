71d24b9990df8eb65155305769f9129442855ff2
박장훈 <gretea4489@gmail.com>
2025-04-02 15:38:06 +0900

    [Feat] UserActivityViewModel 평점 추가 및 수정 로직 추가 #front
    Former-commit-id: 80d5564b3c1fc1bfa95dc9a2e4bc8e860b112caa [formerly e0568ee0af7b5666240a9e1ef3f08abd8c151cda [formerly c62abf6fe344f271bdce4e501f5ea3bf253bd7ef] [formerly 80d5564b3c1fc1bfa95dc9a2e4bc8e860b112caa [formerly c5dea134242fd593dff17eae4488745c1d75d470]]]
    Former-commit-id: e0568ee0af7b5666240a9e1ef3f08abd8c151cda [formerly c62abf6fe344f271bdce4e501f5ea3bf253bd7ef]
    Former-commit-id: e0568ee0af7b5666240a9e1ef3f08abd8c151cda
    Former-commit-id: e8199718e09b172d43017edb4bb59ca2692de8c8

--- app/lib/feature/boardgame/viewmodel/UserActivityViewModel.dart
+++ app/lib/feature/boardgame/viewmodel/UserActivityViewModel.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:jamesboard/main.dart';
 import 'package:jamesboard/repository/UserActivityRepository.dart';
 
+import '../../../datasource/model/request/user/UserActivityPatchRequest.dart';
 import '../../../datasource/model/request/user/UserActivityRequest.dart';
+import '../../../datasource/model/response/user/UserActivityResponse.dart';
 
 class UserActivityViewModel extends ChangeNotifier {
   final UserActivityRepository _repository;
@@ -30,6 +32,64 @@ class UserActivityViewModel extends ChangeNotifier {
     } catch (e) {
       _errorMessage = 'Failed to add user activity: $e';
 
+      return false;
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  Future<int> checkUserActivity({
+    required int userId,
+    required int gameId,
+  }) async {
+    _isLoading = true;
+    _errorMessage = null;
+    notifyListeners();
+
+    logger.d("checkUserActivity: Checking user activity");
+
+    try {
+      final List<UserActivityResponse> activities =
+          await _repository.getUserActivity(userId, gameId);
+
+      logger.d("Activities length: ${activities.length}");
+
+      if (activities.isNotEmpty) {
+        return activities[0].userActivityId;
+      } else {
+        return 0;
+      }
+    } catch (e) {
+      _errorMessage = 'Failed to fetch user activity: $e';
+      return 0;
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  Future<bool> updateUserActivityRating({
+    required int userActivityId,
+    required UserActivityPatchRequest request,
+  }) async {
+    _isLoading = true;
+    _errorMessage = null;
+    notifyListeners();
+
+    logger.d("updateUserActivityRating: Checking user activity");
+
+    try {
+      final id = await _repository.updateUserActivityRating(
+        userActivityId,
+        request,
+      );
+
+      logger.d("updateUserActivityRating: ${id}");
+
+      return id > 0;
+    } catch (e) {
+      _errorMessage = 'Failed to fetch updateUserActivityRating: $e';
       return false;
     } finally {
       _isLoading = false;
