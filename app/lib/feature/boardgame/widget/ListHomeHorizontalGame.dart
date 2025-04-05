0c6110fb902943a2b1402eb387c8f9fd1d08f176
박장훈 <gretea4489@gmail.com>
2025-04-05 19:04:35 +0900

    [Fix] 카테고리 리스트 스크롤 속도 변경 #front
    Former-commit-id: da8dc202279b84299d17b2ed64d03b2dd3ac3c0d [formerly 84eedce25f2f11d4dc23036a6e4f303fc2e838d9 [formerly bcc77901c7d61783d4e57f9bdefef5fa13a676ee] [formerly da8dc202279b84299d17b2ed64d03b2dd3ac3c0d [formerly 7be29658d1cd1ff0a441d4b5d7e8aa75bf537b1b]]]
    Former-commit-id: 84eedce25f2f11d4dc23036a6e4f303fc2e838d9 [formerly bcc77901c7d61783d4e57f9bdefef5fa13a676ee]
    Former-commit-id: 84eedce25f2f11d4dc23036a6e4f303fc2e838d9
    Former-commit-id: 885ca1a9026ad7fa9a434effed27e62aa19f6893

--- app/lib/feature/boardgame/widget/ListHomeHorizontalGame.dart
+++ app/lib/feature/boardgame/widget/ListHomeHorizontalGame.dart
@@ -107,6 +107,7 @@ class _ListHomeHorizontalGameState extends State<ListHomeHorizontalGame> {
                 SizedBox(
                   height: 160,
                   child: ListView.builder(
+                    physics: CustomScrollPhysics(scrollSpeedFactor: 0.4),
                     addAutomaticKeepAlives: true,
                     scrollDirection: Axis.horizontal,
                     itemCount: isLoading ? 5 : games.length,
--- app/lib/widget/physics/CustomScrollPhysics.dart
+++ app/lib/widget/physics/CustomScrollPhysics.dart
@@ -1,21 +1,49 @@
-import 'package:flutter/material.dart';
-import 'package:jamesboard/main.dart';
+import 'dart:io'; // 플랫폼 확인을 위한 패키지
+import 'package:flutter/widgets.dart';
 
 class CustomScrollPhysics extends ScrollPhysics {
   final double scrollSpeedFactor;
 
-  const CustomScrollPhysics({this.scrollSpeedFactor = 1.0})
-      : super(parent: null);
+  const CustomScrollPhysics(
+      {this.scrollSpeedFactor = 1.0, ScrollPhysics? parent})
+      : super(parent: parent);
 
   @override
   CustomScrollPhysics applyTo(ScrollPhysics? ancestor) {
-    return CustomScrollPhysics(scrollSpeedFactor: scrollSpeedFactor);
+    return CustomScrollPhysics(
+      scrollSpeedFactor: scrollSpeedFactor,
+      parent: buildParent(ancestor),
+    );
   }
 
   @override
   double applyPhysicsToUserOffset(ScrollMetrics position, double offset) {
-    logger.d("scrollSpeedFactor: $scrollSpeedFactor");
-    logger.d("Scroll Offset: $offset, Adjusted: ${offset * scrollSpeedFactor}");
     return offset * scrollSpeedFactor;
+  }
+
+  @override
+  Simulation? createBallisticSimulation(
+      ScrollMetrics position, double velocity) {
+    if (velocity.abs() < 10) return null; // 작은 속도는 즉시 정지
+
+    if (Platform.isIOS) {
+      return BouncingScrollSimulation(
+        position: position.pixels,
+        velocity: velocity * scrollSpeedFactor,
+        leadingExtent: position.minScrollExtent,
+        trailingExtent: position.maxScrollExtent,
+        spring: SpringDescription.withDampingRatio(
+          mass: 0.5,
+          stiffness: 100,
+          ratio: 1.1,
+        ),
+      );
+    } else {
+      return ClampingScrollSimulation(
+        position: position.pixels,
+        velocity: velocity * scrollSpeedFactor,
+        friction: 0.015, // 마찰 조절
+      );
+    }
   }
 }
