0d8ca8a790eccd01387f32dc20c42107b5032c9e
kdh4718 <charlie7276@naver.com>
2025-04-06 12:16:05 +0900

    [Fix] BoardGameSearchScreen - 화면 터치 시 키패드 내려가기 #front
    Former-commit-id: 525d0e5ae591d9e25fadb149b038cda0ce51649a [formerly a637a88789ee9bee91c09a84d04922ca16e46f65 [formerly 930874ffc2c5288d7fb1eb537f9fbdc953c4238b] [formerly 525d0e5ae591d9e25fadb149b038cda0ce51649a [formerly cfecc9ea157fb660baf6fdcda4cbd2b32027afd8]]]
    Former-commit-id: a637a88789ee9bee91c09a84d04922ca16e46f65 [formerly 930874ffc2c5288d7fb1eb537f9fbdc953c4238b]
    Former-commit-id: a637a88789ee9bee91c09a84d04922ca16e46f65
    Former-commit-id: 158b3f9d5ddbd63bb82c959c320553c1317eea75

--- app/lib/feature/boardgame/screen/BoardGameSearchScreen.dart
+++ app/lib/feature/boardgame/screen/BoardGameSearchScreen.dart
@@ -63,142 +63,149 @@ class _BoardGameSearchScreenState extends State<BoardGameSearchScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: mainBlack,
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.only(top: 20, left: 20, right: 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // 검색 바 영역
-              SearchBarCommonTitle(
-                controller: _searchController,
-                onSubmitted: (_) => _searchBoardGames(),
-              ),
-              const SizedBox(height: 20),
-
-              // 검색 결과 영역
-              Expanded(
-                child: ChangeNotifierProvider<BoardGameViewModel>.value(
-                  value: boardGameViewModel,
-                  child: Consumer<BoardGameViewModel>(
-                    builder: (context, viewModel, _) {
-                      if (viewModel.isLoading) {
-                        return const Center(child: CircularProgressIndicator());
-                      }
-
-                      final hasSearchResults = viewModel.games.isNotEmpty;
-                      final hasRecentSearches =
-                          viewModel.recentSearches.isNotEmpty;
-
-                      // 검색 결과가 있다면 → 그리드뷰
-                      if (hasSearchResults) {
-                        return GridView.builder(
-                          gridDelegate:
-                              const SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 3,
-                            mainAxisSpacing: 12,
-                            crossAxisSpacing: 12,
-                            childAspectRatio: 3 / 4,
-                          ),
-                          itemCount: viewModel.games.length,
-                          itemBuilder: (context, index) {
-                            final game = viewModel.games[index];
-
-                            return GestureDetector(
-                              onTap: () async {
-                                final selectedGameId = game.gameId;
-                                final selectedGameTitle = game.gameTitle;
-                                final selectedGamePlayTime = game.playTime;
-
-                                if (widget.purpose ==
-                                    BoardGameSearchPurpose.fromHome) {
-                                  viewModel.setSelectedGameId(
-                                      gameId: selectedGameId);
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (_) => BoardGameDetailScreen(
-                                          gameId: selectedGameId),
-                                    ),
-                                  );
-                                } else if (widget.purpose ==
-                                    BoardGameSearchPurpose.fromMission) {
-                                  final missionViewModel =
-                                      context.read<MissionViewModel>();
-
-                                  missionViewModel.setSelectedBoardGame(
-                                    gameId: selectedGameId,
-                                    gameTitle: selectedGameTitle,
-                                    gamePlayTime: selectedGamePlayTime,
-                                  );
-
-                                  Navigator.pop(context);
-                                }
-                              },
-                              child: ImageCommonGameCard(
-                                imageUrl: game.gameImage ?? '',
-                              ),
-                            );
-                          },
-                        );
-                      }
-
-                      // 검색 결과가 없고 최근 검색어가 있으면 → 최근 검색어 리스트
-                      if (hasRecentSearches &&
-                          widget.purpose == BoardGameSearchPurpose.fromHome) {
-                        return Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              '최근 검색',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 18,
-                                fontFamily: FontString.pretendardSemiBold,
-                              ),
-                            ),
-                            const SizedBox(height: 12),
-                            Expanded(
-                              child: ListView.builder(
-                                itemCount: viewModel.recentSearches.length,
-                                itemBuilder: (context, index) {
-                                  final recent =
-                                      viewModel.recentSearches[index];
-
-                                  return ItemCommonRecentSearch(
-                                    title: recent.keyword,
-                                    onTap: () {
-                                      _searchController.text = recent.keyword;
-                                      _searchBoardGames();
-                                    },
-                                    onDelete: () {
-                                      viewModel.deleteRecentSearch(recent.id);
-                                    },
-                                    iconPath: IconPath.close,
-                                  );
-                                },
-                              ),
-                            ),
-                          ],
-                        );
-                      }
-
-                      // ✅ 3. 아무것도 없으면 빈 화면
-                      return const Center(
-                        child: Text(
-                          '검색어를 입력해보세요!',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontFamily: FontString.pretendardSemiBold,
-                            fontSize: 20,
-                          ),
-                        ),
-                      );
-                    },
-                  ),
+      body: GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 20, left: 20, right: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // 검색 바 영역
+                SearchBarCommonTitle(
+                  controller: _searchController,
+                  onSubmitted: (_) => _searchBoardGames(),
                 ),
-              )
-            ],
+                const SizedBox(height: 20),
+
+                // 검색 결과 영역
+                Expanded(
+                  child: ChangeNotifierProvider<BoardGameViewModel>.value(
+                    value: boardGameViewModel,
+                    child: Consumer<BoardGameViewModel>(
+                      builder: (context, viewModel, _) {
+                        if (viewModel.isLoading) {
+                          return const Center(
+                              child: CircularProgressIndicator());
+                        }
+
+                        final hasSearchResults = viewModel.games.isNotEmpty;
+                        final hasRecentSearches =
+                            viewModel.recentSearches.isNotEmpty;
+
+                        // 검색 결과가 있다면 → 그리드뷰
+                        if (hasSearchResults) {
+                          return GridView.builder(
+                            gridDelegate:
+                                const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 3,
+                              mainAxisSpacing: 12,
+                              crossAxisSpacing: 12,
+                              childAspectRatio: 3 / 4,
+                            ),
+                            itemCount: viewModel.games.length,
+                            itemBuilder: (context, index) {
+                              final game = viewModel.games[index];
+
+                              return GestureDetector(
+                                onTap: () async {
+                                  final selectedGameId = game.gameId;
+                                  final selectedGameTitle = game.gameTitle;
+                                  final selectedGamePlayTime = game.playTime;
+
+                                  if (widget.purpose ==
+                                      BoardGameSearchPurpose.fromHome) {
+                                    viewModel.setSelectedGameId(
+                                        gameId: selectedGameId);
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (_) => BoardGameDetailScreen(
+                                            gameId: selectedGameId),
+                                      ),
+                                    );
+                                  } else if (widget.purpose ==
+                                      BoardGameSearchPurpose.fromMission) {
+                                    final missionViewModel =
+                                        context.read<MissionViewModel>();
+
+                                    missionViewModel.setSelectedBoardGame(
+                                      gameId: selectedGameId,
+                                      gameTitle: selectedGameTitle,
+                                      gamePlayTime: selectedGamePlayTime,
+                                    );
+
+                                    Navigator.pop(context);
+                                  }
+                                },
+                                child: ImageCommonGameCard(
+                                  imageUrl: game.gameImage ?? '',
+                                ),
+                              );
+                            },
+                          );
+                        }
+
+                        // 검색 결과가 없고 최근 검색어가 있으면 → 최근 검색어 리스트
+                        if (hasRecentSearches &&
+                            widget.purpose == BoardGameSearchPurpose.fromHome) {
+                          return Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                '최근 검색',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                  fontFamily: FontString.pretendardSemiBold,
+                                ),
+                              ),
+                              const SizedBox(height: 12),
+                              Expanded(
+                                child: ListView.builder(
+                                  itemCount: viewModel.recentSearches.length,
+                                  itemBuilder: (context, index) {
+                                    final recent =
+                                        viewModel.recentSearches[index];
+
+                                    return ItemCommonRecentSearch(
+                                      title: recent.keyword,
+                                      onTap: () {
+                                        _searchController.text = recent.keyword;
+                                        _searchBoardGames();
+                                      },
+                                      onDelete: () {
+                                        viewModel.deleteRecentSearch(recent.id);
+                                      },
+                                      iconPath: IconPath.close,
+                                    );
+                                  },
+                                ),
+                              ),
+                            ],
+                          );
+                        }
+
+                        // ✅ 3. 아무것도 없으면 빈 화면
+                        return Center(
+                          child: Text(
+                            '검색어를 입력해보세요!',
+                            style: TextStyle(
+                              color: mainWhite,
+                              fontFamily: FontString.pretendardSemiBold,
+                              fontSize: 20,
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
