6355f1d932dfb007b284334eada3382dedf7b118
박장훈 <gretea4489@gmail.com>
2025-04-06 13:37:50 +0900

    [Refactor] 문자열 상수 적용 #front
    Former-commit-id: dddd2c2fd0d4ab09d89bf8b347d2b2800418f3a9 [formerly f68ce71f0a12765a119c5f373f4a8c37311bb6a9 [formerly d5ebe7f192582cfecd05875e73086c7fa38a663c] [formerly dddd2c2fd0d4ab09d89bf8b347d2b2800418f3a9 [formerly 73a8a5af383dd090c1ff78238586aa5ae57383ae]]]
    Former-commit-id: f68ce71f0a12765a119c5f373f4a8c37311bb6a9 [formerly d5ebe7f192582cfecd05875e73086c7fa38a663c]
    Former-commit-id: f68ce71f0a12765a119c5f373f4a8c37311bb6a9
    Former-commit-id: eeb9cc1c6408c189687cabaa0490e38d6ba6b264

--- app/lib/constants/AppString.dart
+++ app/lib/constants/AppString.dart
@@ -105,4 +105,87 @@ class AppString {
   static const String apply = "적용";
   //MyPageUserEditScreen
   static const String changeUserName = "변경";
+
+  static const String genrePartyTitle = "파티 : 요원들의 은밀한 모임!";
+  static const String genreStrategyTitle = "전략 : 첩보 전략의 결정판!";
+  static const String genreEconomyTitle = "경제 : 부의 흐름을 추적하라!";
+  static const String genreAdventureTitle = "모험 : 위험과 비밀의 세계!";
+  static const String genreRolePlayingTitle = "롤플레잉 : 위장하고 기만하라!";
+  static const String genreFamilyTitle = "가족 : 웃음과 전략을 함께!";
+  static const String genreMysteryTitle = "추리 : 단서로 배신자를 밝혀라!";
+  static const String genreWarTitle = "전쟁 : 긴장 속 최후의 승자!";
+  static const String genreAbstractStrategyTitle = "추상전략 : 냉철한 전략으로 승부!";
+
+  static const String categoryParty = '파티';
+  static const String categoryStrategy = '전략';
+  static const String categoryEconomy = '경제';
+  static const String categoryAdventure = '모험';
+  static const String categoryRolePlaying = '롤플레잉';
+  static const String categoryFamily = '가족';
+  static const String categoryDeduction = '추리';
+  static const String categoryWar = '전쟁';
+  static const String categoryAbstractStrategy = '추상 전략';
+  static const String categoryAny = '상관없음';
+
+  static const String categoryPartyValue = '파티';
+  static const String categoryStrategyValue = '전략';
+  static const String categoryEconomyValue = '경제';
+  static const String categoryAdventureValue = '모험';
+  static const String categoryRolePlayingValue = '롤플레잉';
+  static const String categoryFamilyValue = '가족';
+  static const String categoryDeductionValue = '추리';
+  static const String categoryWarValue = '전쟁';
+  static const String categoryAbstractStrategyValue = '추상 전략';
+  static const String categoryAnyValue = '상관없음';
+
+  static const String playersSolo = "Solo: 1인";
+  static const String playersDuo = "Duo: 2인";
+  static const String playersTeam = "Team: 3인 이상";
+  static const String playersAssemble = "Assemble: 5인 이상";
+  static const String playersAny = "상관없음";
+
+  static const String playersSoloValue = "1인";
+  static const String playersDuoValue = "2인";
+  static const String playersTeamValue = "3인 이상";
+  static const String playersAssembleValue = "5인 이상";
+
+  static const String difficultyBeginner = "본부, 초급 단계 요청합니다.";
+  static const String difficultyIntermediate = "본부, 중급 단계 요청합니다.";
+  static const String difficultyAdvanced = "본부, 고급 단계 요청합니다.";
+  static const String difficultyAny = "본부, 어느 단계든 상관없습니다.";
+
+  static const String difficultyBeginnerValue = "초급";
+  static const String difficultyIntermediateValue = "중급";
+  static const String difficultyAdvancedValue = "고급";
+  static const String difficultyAnyValue = "상관없음";
+
+  static const String playtimeUltraShort = "초신속 임무 (0 ~ 30분)";
+  static const String playtimeShort = "정밀 작전 (60 ~ 120분)";
+  static const String playtimeMedium = "장기 작전 (120 ~ 240분)";
+  static const String playtimeLong = "마스터 작전 (240분 이상)";
+  static const String playtimeAny = "상관없음";
+
+  static const String playtimeUltraShortValue = "0 ~ 30분";
+  static const String playtimeShortValue = "60 ~ 120분";
+  static const String playtimeMediumValue = "120 ~ 240분";
+  static const String playtimeLongValue = "240분 이상";
+
+  static const String numOfPersonSoloTitle = "Solo Mission : 1명";
+  static const String numOfPersonDuoTitle = "Duo Mission : 2명";
+  static const String numOfPersonTeamTitle = "Team Mission : 3인 이상";
+  static const String numOfPersonAssembleTitle = "Assemble Mission : 5인 이상";
+
+  static const String missionLevelBeginnerTitle = "임무 난이도 : 초급";
+  static const String missionLevelIntermediateTitle = "임무 난이도 : 중급";
+  static const String missionLevelAdvancedTitle = "임무 난이도 : 고급";
+
+  static const String filterGenre = "장르";
+  static const String filterNumOfPersons = "인원";
+  static const String filterDifficulty = "난이도";
+  static const String filterAvgGameTime = "평균 게임 시간";
+
+  static const String keyCategory = "category";
+  static const String keyMinPlayers = "minPlayers";
+  static const String keyDifficulty = "difficulty";
+  static const String keyPlayTime = "playTime";
 }
