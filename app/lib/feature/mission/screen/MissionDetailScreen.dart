1d9fa8e61c36bb0d7163b83060632bfe7d0a44a2
hyuun <wotj28@naver.com>
2025-04-07 14:58:08 +0900

    [Fix]  보드게임 상세 화면 배치 수정 #front
    Former-commit-id: bff60187c175807cb7771419dec794480ca559bf [formerly 5faab2c10c09b5570aa1949d6f8670f09bc5ff2e [formerly 1e373e2edb775298ce716d20a0535e574fab9d9a] [formerly bff60187c175807cb7771419dec794480ca559bf [formerly 36516aa3811f8a99b71e7a12d25aeb6c1a578368]]]
    Former-commit-id: 5faab2c10c09b5570aa1949d6f8670f09bc5ff2e [formerly 1e373e2edb775298ce716d20a0535e574fab9d9a]
    Former-commit-id: 5faab2c10c09b5570aa1949d6f8670f09bc5ff2e
    Former-commit-id: 36b34d34a7acdfda0688b5db0b33dc2ed2c29b39

--- app/lib/feature/mission/screen/MissionDetailScreen.dart
+++ app/lib/feature/mission/screen/MissionDetailScreen.dart
@@ -202,37 +202,43 @@ class _MissionDetailScreenState extends State<MissionDetailScreen> {
             const SizedBox(height: 12),
 
             // 사진 영역 (최대 9장까지 지원)
-            AspectRatio(
-              aspectRatio: 1, // 1:1 비율
-              child: PageView.builder(
-                controller: _pageController,
-                itemCount: archiveDetailResponse.archiveImageList.length ?? 0,
-                itemBuilder: (context, index) {
-                  return Image.network(
-                    archiveDetailResponse
-                        .archiveImageList[index], // 또는 Image.network 가능
-                    fit: BoxFit.cover,
-                  );
-                },
-              ),
-            ),
-
-            const SizedBox(height: 12),
-
-            // 인디케이터
-            if (archiveDetailResponse.archiveImageList.length > 1)
-              SmoothPageIndicator(
-                controller: _pageController,
-                count: archiveDetailResponse.archiveImageList.length,
-                effect: ScaleEffect(
-                  activeDotColor: mainRed,
-                  dotColor: mainGrey,
-                  dotHeight: 6,
-                  dotWidth: 6,
-                  spacing: 6,
-                  scale: 1.5,
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                AspectRatio(
+                  aspectRatio: 1, // 1:1 비율
+                  child: PageView.builder(
+                    controller: _pageController,
+                    itemCount:
+                        archiveDetailResponse.archiveImageList.length ?? 0,
+                    itemBuilder: (context, index) {
+                      return Image.network(
+                        archiveDetailResponse
+                            .archiveImageList[index], // 또는 Image.network 가능
+                        fit: BoxFit.cover,
+                      );
+                    },
+                  ),
                 ),
-              ),
+
+                const SizedBox(height: 12),
+
+                // 인디케이터
+                if (archiveDetailResponse.archiveImageList.length > 1)
+                  SmoothPageIndicator(
+                    controller: _pageController,
+                    count: archiveDetailResponse.archiveImageList.length,
+                    effect: ScaleEffect(
+                      activeDotColor: mainRed,
+                      dotColor: mainGrey,
+                      dotHeight: 6,
+                      dotWidth: 6,
+                      spacing: 6,
+                      scale: 1.5,
+                    ),
+                  ),
+              ],
+            ),
 
             // 게시물 설명
             Padding(
