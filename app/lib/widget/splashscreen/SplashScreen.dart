63ad0772f4e20e1f52f9df3ac887291b8fc5d5a8
hyuun <wotj28@naver.com>
2025-04-07 17:32:22 +0900

    [Fix] Survey 화면에서 나가고 자동 로그인이 되었을 때 다시 Survey 화면으로 오게하기 #front
    Former-commit-id: b7d67053133588b1c1f3170df130aea64aa48f61 [formerly 9da793b7dda49eaace5c19875f8036d7b541a043 [formerly b54c0e3534fb8cb51532ebcd01c624d354c846fc] [formerly b7d67053133588b1c1f3170df130aea64aa48f61 [formerly 6689b2bed8cd8b8ea30e2457cdfb324f0efca4d3]]]
    Former-commit-id: 9da793b7dda49eaace5c19875f8036d7b541a043 [formerly b54c0e3534fb8cb51532ebcd01c624d354c846fc]
    Former-commit-id: 9da793b7dda49eaace5c19875f8036d7b541a043
    Former-commit-id: c5b146c4c8ce022b62976d8ea0f37466e25b39c0

--- app/lib/widget/splashscreen/SplashScreen.dart
+++ app/lib/widget/splashscreen/SplashScreen.dart
@@ -4,8 +4,11 @@ import 'package:jamesboard/constants/AppString.dart';
 import 'package:jamesboard/feature/login/screen/LoginScreen.dart';
 import 'package:jamesboard/theme/Colors.dart';
 import 'package:lottie/lottie.dart';
+import 'package:provider/provider.dart';
 import 'package:shimmer/main.dart';
 
+import '../../feature/login/viewmodel/LoginViewModel.dart';
+import '../../feature/survey/screen/SurveyCategoryScreen.dart';
 import '../../main.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -18,6 +21,50 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  Future<void> _navigate() async {
+    if (widget.isLoggedIn) {
+      final loginViewModel =
+          Provider.of<LoginViewModel>(context, listen: false);
+
+      // storage 에서 userId 읽기
+      final userIdString = await storage.read(key: AppString.keyUserId);
+      final userId = int.tryParse(userIdString ?? '');
+
+      if (userId == null) {
+        // 에러 처리: 로그인은 됐지만 userId 가 없으면 로그인 화면으로
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (_) => const LoginScreen()),
+        );
+        return;
+      }
+
+      // preferBoardGameId 확인
+      final preferBoardGameId =
+          await loginViewModel.checkUserPreferBoardGame(userId);
+
+      if (!mounted) return;
+
+      if (preferBoardGameId == -1) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (_) => const SurveyCategoryScreen()),
+        );
+      } else {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+              builder: (_) => const MyHome(title: AppString.homeAppBarTitle)),
+        );
+      }
+    } else {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const LoginScreen()),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,27 +74,7 @@ class _SplashScreenState extends State<SplashScreen> {
             width: 200,
             height: 200,
             fit: BoxFit.contain, onLoaded: (composition) {
-          Future.delayed(composition.duration, () {
-            // 로그인 된 상태라면
-            if (widget.isLoggedIn) {
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                  builder: (context) =>
-                      const MyHome(title: AppString.homeAppBarTitle),
-                ),
-              );
-            }
-            // 로그인 되지 않은 상태라면
-            else {
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const LoginScreen(),
-                ),
-              );
-            }
-          });
+          _navigate();
         }),
       ),
     );
