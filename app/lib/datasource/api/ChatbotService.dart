ac26ac5a113c341479fb33a5b0266b4408b9081d
박장훈 <gretea4489@gmail.com>
2025-04-09 01:11:58 +0900

    [Fix] ChatbotService baseUrl 설정 및 url 경로 변경 #front
    Former-commit-id: 961e9450f7f1ef6d3c8800c50d19d3a8f7e659fd [formerly 86f17a27d270cfbc55fc6f913cdb67ad460630c2]
    Former-commit-id: e289b1645b01f6d37cfe9e891125b6c3adef2107
    Former-commit-id: 355ce9f86aae97af9ff0d9d442574ab86fd97af6
    Former-commit-id: e38624925cc52f99ba6988eff2bc35c156ef48e1

--- app/lib/datasource/api/ChatbotService.dart
+++ app/lib/datasource/api/ChatbotService.dart
@@ -8,9 +8,10 @@ import '../model/request/chatbot/ChatbotRequest.dart';
 
 part 'ChatbotService.g.dart';
 
+@RestApi(baseUrl: "http://j12d205.p.ssafy.io:9098")
 abstract class ChatbotService {
   factory ChatbotService(Dio dio, {String baseUrl}) = _ChatbotService;
 
-  @POST("fastapi/chat")
+  @POST("/fastapi/chat")
   Future<ChatbotResponse> writeChat(@Body() ChatbotRequest request);
 }
