72847c8e76ddee468def1efda6d029ebb16c6978
박장훈 <gretea4489@gmail.com>
2025-04-09 21:47:49 +0900

    [Fix] BoardGameHomeScreen - 추천 게임 이미지 로딩 속도 지정 #front
    Former-commit-id: 75b49f98019d5e9085157f33767ff4e9836fa7fc [formerly b75eb2e61df3ba0414ccc97f11ef4d53a8379ca9 [formerly 7e85b2870d60a8227df4ae179f11a4da505972ed] [formerly 75b49f98019d5e9085157f33767ff4e9836fa7fc [formerly 54c192b44f99a0ae3ad17dc57ea7790074279e9d]]]
    Former-commit-id: b75eb2e61df3ba0414ccc97f11ef4d53a8379ca9 [formerly 7e85b2870d60a8227df4ae179f11a4da505972ed]
    Former-commit-id: b75eb2e61df3ba0414ccc97f11ef4d53a8379ca9
    Former-commit-id: c00b6114c61f8632fb48cdfaf40e9dcf63214467

--- app/lib/feature/boardgame/widget/CardHomeSuggestion.dart
+++ app/lib/feature/boardgame/widget/CardHomeSuggestion.dart
@@ -35,7 +35,8 @@ class _CardHomeSuggestionState extends State<CardHomeSuggestion> {
     super.initState();
     _pageController = PageController(initialPage: 90000);
     _currentPage = 0;
-    boardGameViewModel = Provider.of<BoardGameViewModel>(context, listen: false);
+    boardGameViewModel =
+        Provider.of<BoardGameViewModel>(context, listen: false);
 
     WidgetsBinding.instance.addPostFrameCallback((_) {
       boardGameViewModel.getRecommendedGames();
@@ -47,7 +48,8 @@ class _CardHomeSuggestionState extends State<CardHomeSuggestion> {
   void _startAutoScroll() {
     _autoScrollTimer?.cancel();
     _autoScrollTimer = Timer.periodic(Duration(seconds: 3), (_) {
-      if (_pageController.hasClients && boardGameViewModel.recommendedGames.isNotEmpty) {
+      if (_pageController.hasClients &&
+          boardGameViewModel.recommendedGames.isNotEmpty) {
         _pageController.nextPage(
           duration: Duration(milliseconds: 500),
           curve: Curves.easeInOut,
@@ -104,91 +106,103 @@ class _CardHomeSuggestionState extends State<CardHomeSuggestion> {
                 height: height,
                 child: isLoading
                     ? Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 32),
-                  child: Shimmer.fromColors(
-                    baseColor: shimmerBaseColor,
-                    highlightColor: shimmerHighlightColor,
-                    child: Container(
-                      width: width,
-                      height: height,
-                      decoration: BoxDecoration(
-                        color: Colors.grey[400],
-                        borderRadius: BorderRadius.circular(4.0),
-                      ),
-                    ),
-                  ),
-                )
-                    : Stack(
-                  children: [
-                    PageView.builder(
-                      controller: _pageController,
-                      itemCount: null, // 무한 스크롤 느낌
-                      onPageChanged: (index) {
-                        setState(() {
-                          _currentPage = index % viewModel.recommendedGames.length;
-                        });
-                      },
-                      itemBuilder: (context, index) {
-                        if (viewModel.recommendedGames.isEmpty) return Container();
-                        final game = viewModel.recommendedGames[index % viewModel.recommendedGames.length];
-                        final imageUrl = game.gameImage;
-                        final id = game.gameId;
-
-                        return GestureDetector(
-                          onTap: () {
-                            _stopAutoScroll();
-                            widget.onImageTap(id);
-                          },
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 32),
-                            child: ClipRRect(
+                        padding: const EdgeInsets.symmetric(horizontal: 32),
+                        child: Shimmer.fromColors(
+                          baseColor: shimmerBaseColor,
+                          highlightColor: shimmerHighlightColor,
+                          child: Container(
+                            width: width,
+                            height: height,
+                            decoration: BoxDecoration(
+                              color: Colors.grey[400],
                               borderRadius: BorderRadius.circular(4.0),
-                              child: CachedNetworkImage(
-                                imageUrl: imageUrl,
-                                width: width,
-                                height: height,
-                                fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                      )
+                    : Stack(
+                        children: [
+                          PageView.builder(
+                            controller: _pageController,
+                            itemCount: null, // 무한 스크롤 느낌
+                            onPageChanged: (index) {
+                              setState(() {
+                                _currentPage =
+                                    index % viewModel.recommendedGames.length;
+                              });
+                            },
+                            itemBuilder: (context, index) {
+                              if (viewModel.recommendedGames.isEmpty)
+                                return Container();
+                              final game = viewModel.recommendedGames[
+                                  index % viewModel.recommendedGames.length];
+                              final imageUrl = game.gameImage;
+                              final id = game.gameId;
+
+                              return GestureDetector(
+                                onTap: () {
+                                  _stopAutoScroll();
+                                  widget.onImageTap(id);
+                                },
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 32),
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(4.0),
+                                    child: CachedNetworkImage(
+                                      imageUrl: imageUrl,
+                                      width: width,
+                                      height: height,
+                                      fit: BoxFit.cover,
+                                      fadeInDuration: Duration(
+                                        microseconds: 500,
+                                      ),
+                                      fadeOutDuration: Duration(
+                                        milliseconds: 500,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                          Positioned(
+                            bottom: 5,
+                            right: 40,
+                            child: Container(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 15, vertical: 10),
+                              decoration: BoxDecoration(
+                                color: Colors.black.withOpacity(0.5),
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                              child: Text.rich(
+                                TextSpan(
+                                  children: [
+                                    TextSpan(
+                                      text: '${_currentPage + 1}',
+                                      style: TextStyle(
+                                        color: mainGold,
+                                        fontSize: 16,
+                                        fontFamily: FontString.pretendardBold,
+                                      ),
+                                    ),
+                                    TextSpan(
+                                      text:
+                                          ' / ${viewModel.recommendedGames.length}',
+                                      style: TextStyle(
+                                        color: mainWhite,
+                                        fontSize: 16,
+                                        fontFamily: FontString.pretendardBold,
+                                      ),
+                                    ),
+                                  ],
+                                ),
                               ),
                             ),
                           ),
-                        );
-                      },
-                    ),
-                    Positioned(
-                      bottom: 5,
-                      right: 40,
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                        decoration: BoxDecoration(
-                          color: Colors.black.withOpacity(0.5),
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        child: Text.rich(
-                          TextSpan(
-                            children: [
-                              TextSpan(
-                                text: '${_currentPage + 1}',
-                                style: TextStyle(
-                                  color: mainGold,
-                                  fontSize: 16,
-                                  fontFamily: FontString.pretendardBold,
-                                ),
-                              ),
-                              TextSpan(
-                                text: ' / ${viewModel.recommendedGames.length}',
-                                style: TextStyle(
-                                  color: mainWhite,
-                                  fontSize: 16,
-                                  fontFamily: FontString.pretendardBold,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
+                        ],
                       ),
-                    ),
-                  ],
-                ),
               );
             },
           ),
